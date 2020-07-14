295908e696bd5697f8d800093fab59c240fcc4ec
Plamen Petrov <plamb0brt@gmail.com>
2020-07-14 08:00:49 -0600

    Updated main module
    Split tests, tests work
    
    Signed-off-by: Plamen Petrov <plamb0brt@gmail.com>

---
+++ failing_test.go
@@ -0,0 +1,121 @@
+// MIT License
+//
+// Copyright (c) 2020 Dmitrii Ustiugov, Plamen Petrov
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestServePauseSnapLoadServe(t *testing.T) {
+	// Needs to be cleaned up manually.
+	fID := "3"
+	imageName := "ustiugov/helloworld:runner_workload"
+	funcPool = NewFuncPool(false, 0, 0, true)
+
+	resp, err := funcPool.Serve(context.Background(), fID, imageName, "world")
+	require.NoError(t, err, "Function returned error on 1st run")
+	require.Equal(t, resp.IsColdStart, true)
+	require.Equal(t, resp.Payload, "Hello, world!")
+
+	_, err = orch.PauseVM(context.Background(), fmt.Sprintf(fID+"_0"))
+	require.NoError(t, err, "Error when pausing VM")
+
+	_, err = orch.CreateSnapshot(context.Background(), fmt.Sprintf(fID+"_0"), "/tmp/snap_test", "/tmp/mem_test")
+	require.NoError(t, err, "Error when creating snapshot of VM")
+
+	_, err = orch.Offload(context.Background(), fmt.Sprintf(fID+"_0"))
+	require.NoError(t, err, "Failed to offload VM, "+"")
+
+	time.Sleep(300 * time.Millisecond)
+
+	_, err = orch.LoadSnapshot(context.Background(), fmt.Sprintf(fID+"_0"), "/tmp/snap_test", "/tmp/mem_test")
+	require.NoError(t, err, "Failed to load snapshot of VM, "+"")
+
+	_, err = orch.ResumeVM(context.Background(), fmt.Sprintf(fID+"_0"))
+	require.NoError(t, err, "Error when resuming VM")
+
+	resp, err = funcPool.Serve(context.Background(), fID, imageName, "world")
+	require.NoError(t, err, "Function returned error on 2nd run")
+	require.Equal(t, resp.Payload, "Hello, world!")
+
+	// Breaks here because removing instance does not work
+	message, err := funcPool.RemoveInstance(fID, imageName)
+	require.NoError(t, err, "Function returned error, "+message)
+}
+
+func TestLoadServeMultiple1(t *testing.T) {
+	// Does not work. Hangs on 3rd serve
+	// Chain:
+	// createVM > serve(1) > pause > createSnap > offload > sleep10
+	// > loadSnap > resume > serve(2) > offload > sleep10 > loadSnap
+	// > resume > serve(3)
+	fID := "4"
+	imageName := "ustiugov/helloworld:runner_workload"
+	funcPool = NewFuncPool(false, 0, 0, true)
+
+	resp, err := funcPool.Serve(context.Background(), fID, imageName, "world")
+	require.NoError(t, err, "Function returned error on 1st run")
+	require.Equal(t, resp.IsColdStart, true)
+	require.Equal(t, resp.Payload, "Hello, world!")
+
+	_, err = orch.PauseVM(context.Background(), fmt.Sprintf(fID+"_0"))
+	require.NoError(t, err, "Error when pausing VM")
+
+	_, err = orch.CreateSnapshot(context.Background(), fmt.Sprintf(fID+"_0"), "/tmp/snap_test", "/tmp/mem_test")
+	require.NoError(t, err, "Error when creating snapshot of VM")
+
+	_, err = orch.Offload(context.Background(), fmt.Sprintf(fID+"_0"))
+	require.NoError(t, err, "Failed to offload VM, "+"")
+
+	time.Sleep(300 * time.Millisecond)
+
+	_, err = orch.LoadSnapshot(context.Background(), fmt.Sprintf(fID+"_0"), "/tmp/snap_test", "/tmp/mem_test")
+	require.NoError(t, err, "Failed to load snapshot of VM, "+"")
+
+	_, err = orch.ResumeVM(context.Background(), fmt.Sprintf(fID+"_0"))
+	require.NoError(t, err, "Error when resuming VM")
+
+	resp, err = funcPool.Serve(context.Background(), fID, imageName, "world")
+	require.NoError(t, err, "Function returned error on 2nd run")
+	require.Equal(t, resp.Payload, "Hello, world!")
+
+	_, err = orch.Offload(context.Background(), fmt.Sprintf(fID+"_0"))
+	require.NoError(t, err, "Failed to offload VM, "+"")
+
+	time.Sleep(300 * time.Millisecond)
+
+	_, err = orch.LoadSnapshot(context.Background(), fmt.Sprintf(fID+"_0"), "/tmp/snap_test", "/tmp/mem_test")
+	require.NoError(t, err, "Failed to load snapshot of VM, "+"")
+
+	_, err = orch.ResumeVM(context.Background(), fmt.Sprintf(fID+"_0"))
+	require.NoError(t, err, "Error when resuming VM")
+
+	resp, err = funcPool.Serve(context.Background(), fID, imageName, "world")
+	require.NoError(t, err, "Function returned error on 3rd run")
+	require.Equal(t, resp.Payload, "Hello, world!")
+}
--- go.mod
+++ go.mod
@@ -10,14 +10,15 @@ replace github.com/firecracker-microvm/firecracker-containerd => github.com/usti
 replace github.com/firecracker-microvm/firecracker-go-sdk => github.com/ustiugov/firecracker-go-sdk v0.20.1-0.20200625102438-8edf287b0123
 
 require (
-	github.com/containerd/containerd v1.3.5-0.20200521195814-e655edce10c9
+	github.com/containerd/containerd v1.3.6
 	github.com/golang/protobuf v1.3.3
-	github.com/sirupsen/logrus v1.5.0
+	github.com/sirupsen/logrus v1.6.0
 	github.com/stretchr/testify v1.5.1
-	github.com/ustiugov/fccd-orchestrator/ctriface v0.0.0-20200710150633-096cac68bd72
-	github.com/ustiugov/fccd-orchestrator/helloworld v0.0.0-20200710150633-096cac68bd72
-	github.com/ustiugov/fccd-orchestrator/misc v0.0.0-20200710150633-096cac68bd72 // indirect
-	github.com/ustiugov/fccd-orchestrator/proto v0.0.0-20200706164751-413344592140
+	github.com/ustiugov/fccd-orchestrator/ctriface v0.0.0-20200714132339-d6594ff6954b
+	github.com/ustiugov/fccd-orchestrator/helloworld v0.0.0-20200714132339-d6594ff6954b
+	github.com/ustiugov/fccd-orchestrator/misc v0.0.0-20200714132339-d6594ff6954b // indirect
+	github.com/ustiugov/fccd-orchestrator/proto v0.0.0-20200714132339-d6594ff6954b
+	github.com/ustiugov/fccd-orchestrator/taps v0.0.0-20200714132339-d6594ff6954b // indirect
 	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208
 	google.golang.org/grpc v1.30.0
 )
