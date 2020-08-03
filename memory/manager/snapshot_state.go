6828384b1a4bc10f01b897b672d76ce0e60c8276
Plamen Petrov <plamb0brt@gmail.com>
2020-08-03 06:28:29 -0600

    one epoller per instance
    Signed-off-by: Plamen Petrov <plamb0brt@gmail.com>

--- memory/manager/snapshot_state.go
+++ memory/manager/snapshot_state.go
@@ -1,23 +1,27 @@
 package manager
 
 import (
-	"os"
-	"sync"
-	"net"
 	"context"
-	"time"
 	"github.com/ftrvxmtrx/fd"
 	log "github.com/sirupsen/logrus"
-	"path/filepath"
 	"golang.org/x/sys/unix"
+	"net"
+	"os"
+	"path/filepath"
+	"sync"
+	"syscall"
+	"time"
+
+	"unsafe"
 )
+
 // SnapshotStateCfg Config to initialize SnapshotState
 type SnapshotStateCfg struct {
-	vmID string
+	vmID                                         string
 	vMMStatePath, guestMemPath, instanceSockAddr string
-	memManagerBaseDir string
-	isRecordMode bool
-	guestMemSize int
+	memManagerBaseDir                            string
+	isRecordMode                                 bool
+	guestMemSize                                 int
 }
 
 // SnapshotState Stores the state of the snapshot
@@ -25,17 +29,20 @@ type SnapshotStateCfg struct {
 type SnapshotState struct {
 	SnapshotStateCfg
 	startAddressOnce *sync.Once // to check if start address has been initialized
-	startAddress uint64
-	baseDir string
-	userFaultFD                                *os.File
-	trace                                    *Trace
+	startAddress     uint64
+	baseDir          string
+	userFaultFD      *os.File
+	userFaultFDInt   int
+	trace            *Trace
+	epfd             int
+	quitCh           chan int
 
 	// install the whole working set in the guest memory
 	isReplayWorkingSet bool
 	// prefetch the VMM state to the host memory
 	isPrefetchVMMState bool
 
-	isWSCopy bool
+	isWSCopy     bool
 	isReplayDone bool
 
 	servedNum int
@@ -53,6 +60,8 @@ func NewSnapshotState(cfg *SnapshotStateCfg) *SnapshotState {
 	s.createDir()
 
 	s.trace = initTrace(s.getTraceFile())
+
+	s.quitCh = make(chan int)
 
 	return s
 }
@@ -93,7 +102,7 @@ func (s *SnapshotState) createDir() {
 }
 
 func (s *SnapshotState) getTraceFile() string {
-	return filepath.Join(s.baseDir, "trace_" + s.vmID)
+	return filepath.Join(s.baseDir, "trace_"+s.vmID)
 }
 
 func (s *SnapshotState) mapGuestMemory() error {
@@ -117,6 +126,82 @@ func (s *SnapshotState) unmapGuestMemory() error {
 		log.Errorf("Failed to munmap guest memory file: %v", err)
 		return err
 	}
-	
+
 	return nil
+}
+
+func (s *SnapshotState) pollUserPageFaults(readyCh chan int) {
+	logger := log.WithFields(log.Fields{"vmID": s.vmID})
+
+	var (
+		events [MaxVMsNum]syscall.EpollEvent
+		err    error
+	)
+
+	logger.Debug("Starting polling loop")
+
+	s.epfd, err = syscall.EpollCreate1(0)
+	if err != nil {
+		logger.Fatalf("epoll_create1: %v", err)
+		os.Exit(1)
+	}
+	defer syscall.Close(s.epfd)
+
+	close(readyCh)
+
+	logger.Debug("Polling loop running")
+
+	for {
+		select {
+		case <-s.quitCh:
+			logger.Debug("Handler received a signal to quit")
+			return
+		default:
+			logger.Debug("Calling epoll_wait")
+			nevents, e := syscall.EpollWait(s.epfd, events[:], -1)
+			if e != nil {
+				logger.Fatalf("epoll_wait: %v", e)
+				break
+			}
+			logger.Debugf("Received %d events\n", nevents)
+
+			if nevents < 1 {
+				panic("Wrong number of events")
+			}
+
+			for i := 0; i < nevents; i++ {
+				event := events[i]
+
+				fd := int(event.Fd)
+				logger.Debugf("Received event for fd=%d\n", fd)
+
+				if fd != s.userFaultFDInt {
+					logger.Fatalf("Received event from unknown fd")
+				}
+
+				address := extractPageFaultAddress(fd)
+
+				s.startAddressOnce.Do(
+					func() {
+						logger.Debug("Received page fault to start address")
+						s.startAddress = address
+					})
+
+				s.servePageFault(fd, address)
+				logger.Debugf("Finished serving page fault\n")
+			}
+		}
+	}
+}
+
+func (s *SnapshotState) servePageFault(fd int, address uint64) {
+	offset := address - s.startAddress
+
+	log.Debugf("Serving offset: %d", offset)
+
+	src := uint64(uintptr(unsafe.Pointer(&s.guestMem[offset])))
+	dst := uint64(int64(address) & ^(int64(pageSize) - 1))
+	mode := uint64(0)
+
+	installRegion(fd, src, dst, mode, 1)
 }
