06ab255d20f3511b9ad0bc5ae13f7d284ab44d9a
Plamen Petrov <plamb0brt@gmail.com>
2020-08-17 07:40:38 -0600

    ctriface bench
    Signed-off-by: Plamen Petrov <plamb0brt@gmail.com>

--- ctriface/bench_test.go
+++ ctriface/bench_test.go
@@ -83,85 +83,8 @@ func TestBenchmarkStart(t *testing.T) {
 			require.NoError(t, err, "Failed to stop VM, "+message)
 		}
 
-		outFileName := "start_" + funcName + ".txt"
-		metrics.PrintMeanStd(getOutFile(outFileName), startMetrics...)
-
-		vmID++
-
-	}
-
-	orch.Cleanup()
-}
-
-func TestBenchmarkLoadResumeWithCache(t *testing.T) {
-	log.SetFormatter(&log.TextFormatter{
-		TimestampFormat: ctrdlog.RFC3339NanoFixed,
-		FullTimestamp:   true,
-	})
-
-	log.SetOutput(os.Stdout)
-
-	log.SetLevel(log.InfoLevel)
-
-	testTimeout := 2000 * time.Second
-	ctx, cancel := context.WithTimeout(namespaces.WithNamespace(context.Background(), namespaceName), testTimeout)
-	defer cancel()
-
-	orch := NewOrchestrator("devmapper", WithTestModeOn(true), WithUPF(*isUPFEnabled))
-
-	images := getAllImages()
-	benchCount := 10
-	vmID := 0
-
-	createResultsDir()
-
-	for funcName, imageName := range images {
-		vmIDString := strconv.Itoa(vmID)
-		loadMetrics := make([]*metrics.Metric, benchCount)
-		resumeMetrics := make([]*metrics.Metric, benchCount)
-
-		// Pull image and prepare snapshot
-		message, _, err := orch.StartVM(ctx, vmIDString, imageName)
-		require.NoError(t, err, "Failed to start VM, "+message)
-
-		message, err = orch.PauseVM(ctx, vmIDString)
-		require.NoError(t, err, "Failed to pause VM, "+vmIDString+", "+message)
-
-		message, err = orch.CreateSnapshot(ctx, vmIDString)
-		require.NoError(t, err, "Failed to create snapshot of VM, "+message)
-
-		message, err = orch.Offload(ctx, vmIDString)
-		require.NoError(t, err, "Failed to offload VM, "+message)
-
-		message, _, err = orch.LoadSnapshot(ctx, vmIDString)
-		require.NoError(t, err, "Failed to load snapshot of VM, "+message)
-
-		message, _, err = orch.ResumeVM(ctx, vmIDString)
-		require.NoError(t, err, "Failed to resume VM, "+message)
-
-		message, err = orch.Offload(ctx, vmIDString)
-		require.NoError(t, err, "Failed to offload VM, "+message)
-
-		for i := 0; i < benchCount; i++ {
-			message, loadMetric, err := orch.LoadSnapshot(ctx, vmIDString)
-			require.NoError(t, err, "Failed to load snapshot of VM, "+message)
-
-			message, resumeMetric, err := orch.ResumeVM(ctx, vmIDString)
-			require.NoError(t, err, "Failed to resume VM, "+message)
-
-			loadMetrics[i] = loadMetric
-			resumeMetrics[i] = resumeMetric
-
-			message, err = orch.Offload(ctx, vmIDString)
-			require.NoError(t, err, "Failed to offload VM, "+message)
-
-		}
-
-		outFileName := "load_" + funcName + "_cache.txt"
-		metrics.PrintMeanStd(getOutFile(outFileName), loadMetrics...)
-
-		outFileName = "load_" + funcName + "_cache.txt"
-		metrics.PrintMeanStd(getOutFile(outFileName), resumeMetrics...)
+		outFileName := "start.txt"
+		metrics.PrintMeanStd(getOutFile(outFileName), funcName, startMetrics...)
 
 		vmID++
 
@@ -237,11 +160,11 @@ func TestBenchmarkLoadResumeNoCache(t *testing.T) {
 
 		}
 
-		outFileName := "load_" + funcName + "_nocache.txt"
-		metrics.PrintMeanStd(getOutFile(outFileName), loadMetrics...)
+		outFileName := "load.txt"
+		metrics.PrintMeanStd(getOutFile(outFileName), funcName, loadMetrics...)
 
-		outFileName = "load_" + funcName + "_nocache.txt"
-		metrics.PrintMeanStd(getOutFile(outFileName), resumeMetrics...)
+		outFileName = "resume.txt"
+		metrics.PrintMeanStd(getOutFile(outFileName), funcName, resumeMetrics...)
 
 		vmID++
 
