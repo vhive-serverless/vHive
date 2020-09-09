6ed784477b1182312600e24c0263e3367306cf5b
Plamen Petrov <plamb0brt@gmail.com>
2020-09-09 08:33:38 -0600

    trivially reduce parallel vms to 1
    Signed-off-by: Plamen Petrov <plamb0brt@gmail.com>

--- manual_cleanup_test.go
+++ manual_cleanup_test.go
@@ -47,7 +47,7 @@ func TestParallelServe(t *testing.T) {
 	require.Equal(t, resp.Payload, "Hello, world!")
 	// -----------------------------------------------------
 
-	vmNum := 3
+	vmNum := 1
 
 	var vmGroup sync.WaitGroup
 	for i := 0; i < vmNum; i++ {
