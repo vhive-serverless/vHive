f3401acb7d00dbd267911e49b757a2c94ccadfa2
Plamen Petrov <plamb0brt@gmail.com>
2020-11-03 12:50:09 +0200

    use istio in k8s script
    Signed-off-by: Plamen Petrov <plamb0brt@gmail.com>

--- scripts/cri/create_kubeadm_cluster.sh
+++ scripts/cri/create_kubeadm_cluster.sh
@@ -2,30 +2,95 @@
 # Create kubelet service
 cat <<EOF > /etc/systemd/system/kubelet.service.d/0-containerd.conf
 [Service]                                                 
-Environment="KUBELET_EXTRA_ARGS=--container-runtime=remote --runtime-request-timeout=15m --container-runtime-endpoint=unix:///etc/firecracker-containerd/fccd-cri.sock"
+Environment="KUBELET_EXTRA_ARGS=--container-runtime=remote --runtime-request-timeout=15m --container-runtime-endpoint=unix:///run/containerd/containerd.sock"
 EOF
 systemctl daemon-reload
 
-kubeadm init --ignore-preflight-errors=all --cri-socket /etc/firecracker-containerd/fccd-cri.sock --pod-network-cidr=192.168.0.0/16
+kubeadm init --ignore-preflight-errors=all --cri-socket /run/containerd/containerd.sock --pod-network-cidr=192.168.0.0/16
 
 # Install Calico network add-on
-kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+curl https://docs.projectcalico.org/manifests/canal.yaml -O
+kubectl apply -f canal.yaml
 
 # Untaint master (allow pods to be scheduled on master) 
 kubectl taint nodes --all node-role.kubernetes.io/master-
+
+# Install and configure MetalLB
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+tee metallb-configmap.yaml <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - 192.168.1.240-192.168.1.250
+EOF
+kubectl apply -f metallb-configmap.yaml
+
+# istio
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.6.11 TARGET_ARCH=x86_64 sh -
+cat << EOF > ./istio-minimal-operator.yaml
+apiVersion: install.istio.io/v1alpha1
+kind: IstioOperator
+spec:
+  values:
+    global:
+      proxy:
+        autoInject: disabled
+      useMCP: false
+      # The third-party-jwt is not enabled on all k8s.
+      # See: https://istio.io/docs/ops/best-practices/security/#configure-third-party-service-account-tokens
+      jwtPolicy: first-party-jwt
+
+  addonComponents:
+    pilot:
+      enabled: true
+    prometheus:
+      enabled: false
+
+  components:
+    ingressGateways:
+      - name: istio-ingressgateway
+        enabled: true
+      - name: cluster-local-gateway
+        enabled: true
+        label:
+          istio: cluster-local-gateway
+          app: cluster-local-gateway
+        k8s:
+          service:
+            type: ClusterIP
+            ports:
+            - port: 15020
+              name: status-port
+            - port: 80
+              name: http2
+            - port: 443
+              name: https
+EOF
+
+./istio-1.6.11/bin/istioctl manifest apply -f istio-minimal-operator.yaml
 
 
 # Install KNative in the cluster
 kubectl apply --filename https://github.com/knative/serving/releases/download/v0.17.0/serving-crds.yaml
 kubectl apply --filename https://github.com/knative/serving/releases/download/v0.17.0/serving-core.yaml
 
-# Configure network
-kubectl apply --filename https://raw.githubusercontent.com/Kong/kubernetes-ingress-controller/0.9.x/deploy/single/all-in-one-dbless.yaml
-kubectl patch configmap/config-network \
-  --namespace knative-serving \
-  --type merge \
-  --patch '{"data":{"ingress.class":"kong"}}'
+# magic DNS
+kubectl apply --filename https://github.com/knative/serving/releases/download/v0.18.0/serving-default-domain.yaml
 
-# Path load balancer
-PUBLIC_IP=$(curl ifconfig.me)
-kubectl patch svc kong-proxy -n kong -p '{"spec": {"type": "LoadBalancer", "externalIPs":["'${PUBLIC_IP}'"]}}'
+kubectl apply --filename https://github.com/knative/net-istio/releases/download/v0.18.0/release.yaml
+kubectl --namespace istio-system get service istio-ingressgateway
