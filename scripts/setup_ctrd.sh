70919d06397faddabbbd04f617578e5648e47eb3
Plamen Petrov <plamb0brt@gmail.com>
2020-11-04 18:39:02 +0200

    copy binaries in travis
    Signed-off-by: Plamen Petrov <plamb0brt@gmail.com>

--- scripts/setup_ctrd.sh
+++ scripts/setup_ctrd.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #set -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 sudo apt-get update 2>&1
 
 sudo apt-get -y install \
@@ -9,54 +11,40 @@ sudo apt-get -y install \
 	curl \
 	gnupg-agent \
 	dmsetup \
+  git-lfs \
 	software-properties-common 2>&1
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo apt-key fingerprint 0EBFCD88
-
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-sudo apt-get update
-
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 # enable KVM
 sudo setfacl -m u:${USER}:rw /dev/kvm
 sudo modprobe msr
 
+git lfs pull
+BINS=./bin
+DST=/usr/local/bin
 
-# firecracker-containerd and thinpool for devmapper
 
-err=""; \
-    [ "$(uname) $(uname -m)" = "Linux x86_64" ] \
-    || err="ERROR: your system is not Linux x86_64."; \
-    [ -r /dev/kvm ] && [ -w /dev/kvm ] \
-    || err="$err\nERROR: /dev/kvm is innaccessible."; \
-    (( $(uname -r | cut -d. -f1)*1000 + $(uname -r | cut -d. -f2) >= 4014 )) \
-    || err="$err\nERROR: your kernel version ($(uname -r)) is too old."; \
-    dmesg | grep -i "hypervisor detected" \
-    && echo "WARNING: you are running in a virtual machine. Firecracker is not well tested under nested virtualization."; \
-    [ -z "$err" ] && echo "Your system looks ready for Firecracker!" || echo -e "$err"
-
-git clone -b upf_master --recurse-submodules https://github.com/ustiugov/firecracker-containerd
-
-echo Configure firecracker-containerd
-pushd firecracker-containerd > /dev/null
-curl -fsSL -o hello-vmlinux.bin https://s3.amazonaws.com/spec.ccfc.min/img/hello/kernel/hello-vmlinux.bin
-make all
-make firecracker
-make image
-sudo make install install-firecracker demo-network
 sudo mkdir -p /var/lib/firecracker-containerd/runtime
-sudo cp tools/image-builder/rootfs.img /var/lib/firecracker-containerd/runtime/default-rootfs.img
 sudo mkdir -p /etc/firecracker-containerd
 sudo mkdir -p /var/lib/firecracker-containerd/containerd
+sudo mkdir -p /etc/containerd
 
 sudo mkdir -p /var/lib/firecracker-containerd
+
+pushd $BINS
+
+sudo cp firecracker $DST
+sudo cp jailer $DST
+sudo cp containerd-shim-aws-firecracker $DST
+sudo cp firecracker-containerd $DST
+sudo cp firecracker-ctr $DST
+sudo cp default-rootfs.img /var/lib/firecracker-containerd/runtime/
+popd 
+
+echo Configure firecracker-containerd
+curl -fsSL -o hello-vmlinux.bin https://s3.amazonaws.com/spec.ccfc.min/img/hello/kernel/hello-vmlinux.bin
+sudo mv ./hello-vmlinux.bin /var/lib/firecracker-containerd/runtime/default-vmlinux.bin
+
 sudo tee /etc/firecracker-containerd/config.toml <<EOF
 disabled_plugins = ["cri"]
 root = "/var/lib/firecracker-containerd/containerd"
@@ -73,11 +61,8 @@ state = "/run/firecracker-containerd"
 EOF
 
 # Setup device mapper thin pool
-../scripts/create_devmapper.sh
+./scripts/create_devmapper.sh
 
-sudo mkdir -p /var/lib/firecracker-containerd/runtime
-sudo mv ./hello-vmlinux.bin /var/lib/firecracker-containerd/runtime/default-vmlinux.bin
-sudo mkdir -p /etc/containerd
 sudo tee /etc/containerd/firecracker-runtime.json <<EOF
 {
   "firecracker_binary_path": "/usr/local/bin/firecracker",
@@ -88,4 +73,3 @@ sudo tee /etc/containerd/firecracker-runtime.json <<EOF
   "kernel_args": "console=ttyS0 noapic reboot=k panic=1 pci=off nomodules ro systemd.journald.forward_to_console systemd.unit=firecracker.target init=/sbin/overlay-init tsc=reliable quiet 8250.nr_uarts=0 ipv6.disable=1"
 }
 EOF
-popd > /dev/null
