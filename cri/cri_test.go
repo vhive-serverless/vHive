4c0a680212016eead4aa5aa868edeb32a6e7bb31
Plamen Petrov <plamb0brt@gmail.com>
2020-11-05 14:10:17 +0200

    add bench in cri
    Signed-off-by: Plamen Petrov <plamb0brt@gmail.com>

--- cri/cri_test.go
+++ cri/cri_test.go
@@ -23,6 +23,10 @@
 package cri
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
 	"testing"
 
 	"sync"
@@ -38,6 +42,38 @@ import (
 var functionURL string = "f1.default.192.168.1.240.xip.io:80"
 
 func TestSingleCall(t *testing.T) {
+	invoke(t, functionURL)
+}
+
+func TestParallelCall(t *testing.T) {
+	n := 50
+	var wg sync.WaitGroup
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			invoke(t, functionURL)
+		}()
+	}
+	wg.Wait()
+}
+
+func TestBench(t *testing.T) {
+	dropPageCache()
+	start := time.Now()
+	invoke(t, functionURL)
+	end := time.Since(start)
+	fmt.Printf("First invocation took %d ms\n", end.Milliseconds())
+
+	dropPageCache()
+	start = time.Now()
+	invoke(t, functionURL)
+	end = time.Since(start)
+	fmt.Printf("Second invocation took %d ms\n", end.Milliseconds())
+}
+
+func invoke(t *testing.T, functionURL string) {
 	client, conn, err := getClient(functionURL)
 	require.NoError(t, err, "Failed to dial function URL")
 	defer conn.Close()
@@ -48,31 +84,20 @@ func TestSingleCall(t *testing.T) {
 	require.NoError(t, err, "Failed to get response from function")
 }
 
-func TestParallelCall(t *testing.T) {
-	n := 5
-	var wg sync.WaitGroup
-
-	for i := 0; i < n; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			client, conn, err := getClient(functionURL)
-			require.NoError(t, err, "Failed to dial function URL")
-			defer conn.Close()
-			ctxFwd, cancel := context.WithDeadline(context.Background(), time.Now().Add(20*time.Second))
-			defer cancel()
-
-			_, err = client.SayHello(ctxFwd, &hpb.HelloRequest{Name: "record"})
-			require.NoError(t, err, "Failed to get response from function")
-		}()
-	}
-	wg.Wait()
-}
-
 func getClient(functionURL string) (hpb.GreeterClient, *grpc.ClientConn, error) {
 	conn, err := grpc.Dial(functionURL, grpc.WithInsecure())
 	if err != nil {
 		return nil, nil, err
 	}
 	return hpb.NewGreeterClient(conn), conn, nil
+}
+
+func dropPageCache() {
+	cmd := exec.Command("sudo", "/bin/sh", "-c", "sync; echo 1 > /proc/sys/vm/drop_caches")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Failed to drop caches: %v", err)
+	}
 }
