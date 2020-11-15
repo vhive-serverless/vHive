b0ace69b913724be8a0654e5a43ecee0828ec9c5
NiuJ1ao <NiuJ1ao@github.com>
2020-11-15 20:24:47 +0800

    remove comments and add imageName to parameters of goroutine.

--- perf_bench_test.go
+++ perf_bench_test.go
@@ -41,9 +41,6 @@ var (
 )
 
 func TestBenchRequestPerSecond(t *testing.T) {
-	// TODO:
-	// 1. Pulling images and creating snapshots, creating records must be outside of the measurement loop. In fact, the task was to profile warm invocations, not cold. Hence, snapshots, records are unnecessary at this point (but you can keep them for future).
-	// 2. Place remove instance and flush caches calls under a runtime argument profile_cold_starts that you need to add
 
 	var (
 		servedTh       uint64
@@ -213,7 +210,7 @@ func loadAndProfile(t *testing.T, images []string, vmNum, targetRPS int, isSyncO
 
 				tStart := time.Now()
 
-				go func(start time.Time, vmIDString string, semaphore chan bool) {
+				go func(start time.Time, vmIDString, imageName string, semaphore chan bool) {
 					defer func() { <-semaphore }()
 
 					// serve
@@ -233,7 +230,7 @@ func loadAndProfile(t *testing.T, images []string, vmNum, targetRPS int, isSyncO
 					log.Printf("timeLeft: %f seconds", float64(timeLeft)*1e-9)
 
 					time.Sleep(time.Duration(timeLeft))
-				}(tStart, vmIDString, sem)
+				}(tStart, vmIDString, imageName, sem)
 
 				funcIdx++
 				funcIdx %= len(funcs)
