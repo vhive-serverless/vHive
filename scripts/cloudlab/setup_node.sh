ea8b1d31c637f42871b3b94c6c30ae8e6974b998
Plamen Petrov <plamb0brt@gmail.com>
2020-11-20 12:20:40 +0200

    individual cloudlab script, remove k8s from travis
    Signed-off-by: Plamen Petrov <plamb0brt@gmail.com>

--- scripts/cloudlab/setup_node.sh
+++ scripts/cloudlab/setup_node.sh
@@ -3,9 +3,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT="$( cd $DIR && cd .. && cd .. && pwd)"
 SCRIPTS=$ROOT/scripts
 
+source $SCRIPTS/install_go.sh
 $SCRIPTS/setup_system.sh
 $SCRIPTS/setup_containerd.sh
 
-#$SCRIPTS/install_stock.sh // Remove because we are not testing cri in travis
+$SCRIPTS/install_stock.sh
 $SCRIPTS/create_devmapper.sh
 
