6ae085e205dbdf3023f70b4722251c03f9ffd2c3
Plamen Petrov <plamb0brt@gmail.com>
2020-11-20 11:53:02 +0200

    keep configs under git
    Signed-off-by: Plamen Petrov <plamb0brt@gmail.com>

--- scripts/cluster/setup_worker_kubelet.sh
+++ scripts/cluster/setup_worker_kubelet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Create kubelet service
-cat <<EOF > /etc/systemd/system/kubelet.service.d/0-containerd.conf
+sudo sh -c 'cat <<EOF > /etc/systemd/system/kubelet.service.d/0-containerd.conf
 [Service]                                                 
 Environment="KUBELET_EXTRA_ARGS=--container-runtime=remote --runtime-request-timeout=15m --container-runtime-endpoint=unix:///etc/firecracker-containerd/fccd-cri.sock"
-EOF
-systemctl daemon-reload
+EOF'
+sudo systemctl daemon-reload
