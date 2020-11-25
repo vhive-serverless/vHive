ff51f9f4273679685af8411b13e4f41184c5b3aa
Author NiuJ1ao <26167136+NiuJ1ao@users.noreply.github.com>
2020-11-26 00:37:02 +0800

    change boot VMs back to sequential due to data race.
    Signed-off-by: NiuJ1ao <26167136+NiuJ1ao@users.noreply.github.com>

--- perf_bench_test.go
+++ perf_bench_test.go
@@ -82,19 +82,13 @@ func TestBenchRequestPerSecond(t *testing.T) {
 	}
 
 	funcsLen := len(funcs)
-	var vmGroup sync.WaitGroup
-	vmGroup.Add(*vmNum)
 
 	// Boot VMs
 	for i := 0; i < *vmNum; i++ {
 		vmIDString := strconv.Itoa(i)
-		go func(vmIDString string, funcIdx int) {
-			defer vmGroup.Done()
-			_, err := funcPool.AddInstance(vmIDString, images[funcs[funcIdx]])
-			require.NoError(t, err, "Function returned error")
-		}(vmIDString, i%funcsLen)
+		_, err := funcPool.AddInstance(vmIDString, images[funcs[i%funcsLen]])
+		require.NoError(t, err, "Function returned error")
 	}
-	vmGroup.Wait()
 
 	if !*isWithCache && *isColdStart {
 		log.Info("Profile cold start")
@@ -105,8 +99,6 @@ func TestBenchRequestPerSecond(t *testing.T) {
 
 	done := make(chan bool, 1)
 	var vmGroup sync.WaitGroup
-	ticker := time.NewTicker(timeInterval)
-	log.Debugf("All VMs booted in %d ms", time.Since(bootStart).Milliseconds())
 
 	for totalRequests > 0 {
 		select {
@@ -154,14 +146,14 @@ func serveVM(t *testing.T, start time.Time, vmIDString, imageName string, vmGrou
 
 func getImages() map[string]string {
 	return map[string]string{
-		"helloworld":   "ustiugov/helloworld:var_workload",
-		"chameleon":    "ustiugov/chameleon:var_workload",
-		"pyaes":        "ustiugov/pyaes:var_workload",
-		"image_rotate": "ustiugov/image_rotate:var_workload",
-		"json_serdes":  "ustiugov/json_serdes:var_workload",
-		"lr_serving":   "ustiugov/lr_serving:var_workload",
-		"cnn_serving":  "ustiugov/cnn_serving:var_workload",
-		"rnn_serving":  "ustiugov/rnn_serving:var_workload",
-		"lr_training":  "ustiugov/lr_training:var_workload",
+		"helloworld": "ustiugov/helloworld:var_workload",
+		// "chameleon":    "ustiugov/chameleon:var_workload",
+		// "pyaes":        "ustiugov/pyaes:var_workload",
+		// "image_rotate": "ustiugov/image_rotate:var_workload",
+		// "json_serdes":  "ustiugov/json_serdes:var_workload",
+		// "lr_serving":   "ustiugov/lr_serving:var_workload",
+		// "cnn_serving":  "ustiugov/cnn_serving:var_workload",
+		// "rnn_serving":  "ustiugov/rnn_serving:var_workload",
+		// "lr_training":  "ustiugov/lr_training:var_workload",
 	}
 }
