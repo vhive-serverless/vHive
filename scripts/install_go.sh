a46108e3b46b7633568ac91b15f8a1ff910a9fa0
Dmitrii Ustiugov <dmitrii.ustiugov@epfl.ch>
2020-12-01 14:00:32 +0000

    Remove ssh authentication
    HTTPS authentication does not require a password

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -32,7 +32,3 @@ sudo tar -C /usr/local -xzf go1.14.6.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 sudo sh -c  "echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile"
-
-# use ssh for git, set up GOPRIVATE
-git config --global url."git@github.com:".insteadOf https://github.com/
-go env -w GOPRIVATE=github.com/ustiugov/*
