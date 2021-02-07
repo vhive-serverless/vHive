9ba3e7a141bb583822496575a3577df732dee5c5
NiuJ1ao <26167136+NiuJ1ao@users.noreply.github.com>
2021-02-08 00:34:23 +0800

    add install pmu-tools to github actions and minor fix bugs in the script
    Signed-off-by: NiuJ1ao <26167136+NiuJ1ao@users.noreply.github.com>

--- scripts/install_pmutool.sh
+++ scripts/install_pmutool.sh
@@ -6,6 +6,7 @@ sudo apt-get install linux-tools-`uname -r` -y
 
 git clone https://github.com/andikleen/pmu-tools /usr/local/pmu-tools
 
+export PATH=$PATH:/usr/local/pmu-tools
 sudo sh -c  "echo 'export PATH=\$PATH:/usr/local/pmu-tools' >> /etc/profile"
 sudo sh -c  "echo 'echo 0 > /proc/sys/kernel/nmi_watchdog' >> /etc/profile"
 
