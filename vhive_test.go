adec776aa24f4ad5ce42f6ab77315ca86c195020
Theodor Amariucai <32778667+amariucaitheodor@users.noreply.github.com>
2021-02-24 16:07:13 +0000

    Update vhive_test.go

--- vhive_test.go
+++ vhive_test.go
@@ -294,6 +294,7 @@ func TestAllFunctions(t *testing.T) {
 		"vhiveease/cnn_serving:var_workload",
 		"vhiveease/rnn_serving:var_workload",
 		"vhiveease/lr_training:var_workload",
+		"vhiveease/springboot:var_workload",
 	}
 	var (
 		servedTh      uint64
