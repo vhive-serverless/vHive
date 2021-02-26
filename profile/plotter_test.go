d911caa28ea5a94ade0ad7dde7a64bcb28aceb20
NiuJ1ao <26167136+NiuJ1ao@users.noreply.github.com>
2021-02-27 01:26:46 +0800

    make x-axis configurable
    Signed-off-by: NiuJ1ao <26167136+NiuJ1ao@users.noreply.github.com>

--- profile/plotter_test.go
+++ profile/plotter_test.go
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2021 Yuchen Niu and EASE lab
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package profile
 
 import (
@@ -36,7 +58,7 @@ func TestCreatingPlotter(t *testing.T) {
 	err := createTestFile(fileName)
 	require.NoError(t, err, "Failed creating test file")
 
-	CSVPlotter(4, "", fileName)
+	PlotCVS(4, "", fileName, "X-axis")
 
 	plotNames := []string{"field1.png", "field2.png", "field-3.png"}
 	for _, fname := range plotNames {
