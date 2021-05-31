f8a22b8b6e06b75c74c5f6bd175197a50fa376d6
Bora M. Alper <bora@boramalper.org>
2021-05-31 12:48:48 +0300

    pin Knative client to v0.21.0
    Signed-off-by: Bora M. Alper <bora@boramalper.org>

--- scripts/install_stock.sh
+++ scripts/install_stock.sh
@@ -47,7 +47,8 @@ sudo apt-get update >> /dev/null
 sudo apt-get -y install cri-tools ebtables ethtool kubeadm=$K8S_VERSION kubectl=$K8S_VERSION kubelet=$K8S_VERSION kubernetes-cni >> /dev/null
 
 # Install knative CLI
-git clone --quiet --depth=1 https://github.com/knative/client.git $HOME/client
+KNATIVE_VERSION=v0.21.0
+git clone --quiet --depth=1 --branch=$KNATIVE_VERSION -c advice.detachedHead=false https://github.com/knative/client.git $HOME/client
 cd $HOME/client
 hack/build.sh -f
 sudo mv kn /usr/local/bin
