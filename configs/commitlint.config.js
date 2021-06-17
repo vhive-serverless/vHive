67b50287ef17f477c40e85fec2d99ed319e5eb88
Shyam Jesal <s.jesalpura@gmail.com>
2021-06-17 17:01:11 +0530

    change warning to error on linting unsigned commit
    Signed-off-by: Shyam Jesal <s.jesalpura@gmail.com>

--- configs/commitlint.config.js
+++ configs/commitlint.config.js
@@ -1,11 +1,21 @@
 const Configuration = {
+
+    // The configuration array contains:
+
+    // Level [0..2]: 0 disables the rule. For 1 it will be considered a warning for 2 an error.
+    // Applicable [always|never]: never inverts the rule.
+    // Value: value to use for this rule.
+    // Source:
+    // https://commitlint.js.org/#/reference-rules
+
     /*
      * Any rules defined here will override rules from @commitlint/config-conventional
      */
+
     rules: {
-        "header-max-length": [0, "always", 72],
-        "header-min-length": [0, "always", 10],
-        'signed-off-by': [1, 'always', 'Signed-off-by:'],
+        "header-max-length": [2, "always", 72],
+        "header-min-length": [2, "always", 10],
+        'signed-off-by': [2, 'always', 'Signed-off-by:'],
     },
 };
 
