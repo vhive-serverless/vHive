6a15552ef686f193927465ad2f7f4a33f3fefba8
Bora M. Alper <bora@boramalper.org>
2021-06-23 15:48:09 +0300

    invoke workflows serially (rather than in parallel)
    Signed-off-by: Bora M. Alper <bora@boramalper.org>

--- examples/invoker/measure.go
+++ examples/invoker/measure.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	"github.com/golang/protobuf/ptypes/empty"
@@ -12,19 +13,29 @@ import (
 	"eventing/proto"
 )
 
-func Start(tdbAddr string, matchers map[string]string) (End func() time.Duration) {
+var (
+	conn   *grpc.ClientConn
+	client proto.TimeseriesClient
+	lock   sync.Mutex
+)
+
+func Start(tdbAddr string, matchers map[string]string) {
+	lock.Lock()
+	defer lock.Unlock()
+
 	var dialOption grpc.DialOption
 	if *withTracing {
 		dialOption = grpc.WithUnaryInterceptor(otelgrpc.UnaryClientInterceptor())
 	} else {
 		dialOption = grpc.WithBlock()
 	}
-	conn, err := grpc.Dial(tdbAddr, grpc.WithInsecure(), dialOption)
+	var err error
+	conn, err = grpc.Dial(tdbAddr, grpc.WithInsecure(), dialOption)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 
-	client := proto.NewTimeseriesClient(conn)
+	client = proto.NewTimeseriesClient(conn)
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
@@ -37,20 +48,25 @@ func Start(tdbAddr string, matchers map[string]string) (End func() time.Duration
 	}); err != nil {
 		log.Fatalln("failed to start experiment", err)
 	}
+}
 
-	return func() time.Duration {
-		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-		defer cancel()
-		res, err := client.EndExperiment(ctx, &empty.Empty{})
-		if err != nil {
-			log.Fatalln("failed to end experiment", err)
-		}
-		if len(res.Invocations) != 1 {
-			log.Fatalf("wrong number of invocations: 1 != %d\n", len(res.Invocations))
-		}
-		if err := conn.Close(); err != nil {
-			log.Warnln("failed to close tdb connection", err)
-		}
-		return res.Invocations[0].Duration.AsDuration()
+func End() (durations []time.Duration) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	defer conn.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	res, err := client.EndExperiment(ctx, &empty.Empty{})
+	if err != nil {
+		log.Fatalln("failed to end experiment", err)
 	}
+	for _, inv := range res.Invocations {
+		// Skip incomplete invocations
+		if inv.Status != proto.InvocationStatus_COMPLETED {
+			continue
+		}
+		durations = append(durations, inv.Duration.AsDuration())
+	}
+	return
 }
