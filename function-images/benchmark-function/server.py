a3c78cf13d409e3268677d666fe8f108333cb1ca
yw13579 <77960797+yw13579@users.noreply.github.com>
2021-07-12 13:43:59 -0700

    Update server.py

--- function-images/benchmark-function/server.py
+++ function-images/benchmark-function/server.py
@@ -1,4 +1,3 @@
-
 import grpc
 import time
 import json
@@ -6,23 +5,25 @@ import logging
 from concurrent import futures
 import helloworld_pb2
 import helloworld_pb2_grpc
-
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
     def SayHello(self, request, context):
         userinput = json.loads(request.name)
         targettime = userinput['executiontime']
         timeout = time.time() + targettime * 0.001
-        #while time.time() < timeout:                                                        
-           # dummy = 1 + 1                                                                   
-        return helloworld_pb2.HelloReply(message=msg)
-      
+        print(str(timeout))
+        while time.time() < timeout:
+            dummy = 1 + 1
+        return helloworld_pb2.HelloReply(message='testing')
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    print('hello1')
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    print('hello2')
     server.add_insecure_port('[::]:50051')
+    print('hello3')
     server.start()
+    print('hello4')
     server.wait_for_termination()
-    
+    print('terminated')
 if __name__ == '__main__':
     logging.basicConfig()
-    serve()
