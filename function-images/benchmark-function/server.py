4cffbfc7f18ac0239c81612db6f1e557b957550d
yw13579 <77960797+yw13579@users.noreply.github.com>
2021-07-14 14:24:45 -0700

    Update server.py

--- function-images/benchmark-function/server.py
+++ function-images/benchmark-function/server.py
@@ -1,5 +1,3 @@
-#1626122012.2109742 output, to be interpreted.
-
 import grpc
 import time
 import json
@@ -12,20 +10,17 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
         userinput = json.loads(request.name)
         targettime = userinput['executiontime']
         timeout = time.time() + targettime * 0.001
-        print(str(timeout))
         while time.time() < timeout:
             dummy = 1 + 1
+        msg = 'sup' + str(timeout)
+        print(msg)
         return helloworld_pb2.HelloReply(message='testing')
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
-    print('hello1')
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
-    print('hello2')
     server.add_insecure_port('[::]:50051')
-    print('hello3')
     server.start()
-    print('hello4')
     server.wait_for_termination()
-    print('terminated')
 if __name__ == '__main__':
     logging.basicConfig()
+    serve()
