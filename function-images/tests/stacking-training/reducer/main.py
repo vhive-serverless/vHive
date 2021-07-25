660a8264c099e81bfe7cd5364eb02fbbb63e8dac
Dmitrii Ustiugov <dmitrii.ustiugov@epfl.ch>
2021-07-25 14:29:33 +0200

    initial commit for stacking-training
    Signed-off-by: Dmitrii Ustiugov <dmitrii.ustiugov@epfl.ch>

---
+++ function-images/tests/stacking-training/reducer/main.py
@@ -0,0 +1,200 @@
+# MIT License
+#
+# Copyright (c) 2021 EASE lab
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import print_function
+
+import sys
+import os
+import grpc
+import argparse
+import boto3
+import logging as log
+import socket
+
+import numpy as np
+import pickle
+
+from concurrent import futures
+
+# adding python tracing sources to the system path
+sys.path.insert(0, os.getcwd() + '/../proto/')
+sys.path.insert(0, os.getcwd() + '/../../../../utils/tracing/python')
+import tracing
+import stacking_pb2_grpc
+import stacking_pb2
+import destination as XDTdst
+import source as XDTsrc
+import utils as XDTutil
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-dockerCompose", "--dockerCompose", dest="dockerCompose", default=False, help="Env docker compose")
+parser.add_argument("-sp", "--sp", dest="sp", default="80", help="serve port")
+parser.add_argument("-zipkin", "--zipkin", dest="zipkinURL",
+                    default="http://zipkin.istio-system.svc.cluster.local:9411/api/v2/spans",
+                    help="Zipkin endpoint url")
+
+args = parser.parse_args()
+
+if tracing.IsTracingEnabled():
+    tracing.initTracer("reducer", url=args.zipkinURL)
+    tracing.grpcInstrumentClient()
+    tracing.grpcInstrumentServer()
+
+INLINE = "INLINE"
+S3 = "S3"
+XDT = "XDT"
+
+# set aws credentials:
+AWS_ID = os.getenv('AWS_ACCESS_KEY', "")
+AWS_SECRET = os.getenv('AWS_SECRET_KEY', "")
+
+
+def get_self_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+
+def model_dispatcher(model_name):
+    if model_name == 'LinearSVR':
+        return LinearSVR
+    elif model_name == 'Lasso':
+        return Lasso
+    elif model_name == 'LinearRegression':
+        return LinearRegression
+    elif model_name == 'RandomForestRegressor':
+        return RandomForestRegressor
+    elif model_name == 'KNeighborsRegressor':
+        return KNeighborsRegressor
+    elif model_name == 'LogisticRegression':
+        return LogisticRegression
+    else:
+        raise ValueError(f"Model {model_name} not found")
+
+
+class ReducerServicer(stacking_pb2_grpc.ReducerServicer):
+    def __init__(self, transferType, XDTconfig=None):
+
+        self.benchName = 'vhive-stacking'
+        self.transferType = transferType
+        if transferType == S3:
+            self.s3_client = boto3.resource(
+                service_name='s3',
+                region_name=os.getenv("AWS_REGION", 'us-west-1'),
+                aws_access_key_id=AWS_ID,
+                aws_secret_access_key=AWS_SECRET
+            )
+        elif transferType == XDT:
+            if XDTconfig is None:
+                log.fatal("Empty XDT config")
+            self.XDTconfig = XDTconfig
+
+    def put_to_s3(self, obj, key):
+        s3object = self.s3_client.Object(bucket_name=self.benchName, key=key)
+        pickled = pickle.dumps(obj)
+        s3object.put(Body=pickled)
+
+    def get_from_s3(self, key):
+        obj = self.s3_client.Object(bucket_name=self.benchName, key=key)
+        response = obj.get()
+        return pickle.loads(response['Body'].read())
+
+    def Reduce(self, request, context):
+        log.info(f"Reducer is invoked")
+
+        models = []
+        predictions = []
+        if self.transferType == S3:
+            log.info("Reducer gets the models from S3")
+            with tracing.Span("Reducer gets models from S3"):
+                for i, model_pred_tuple in enumerate(request.model_pred_tuples):
+                    with tracing.Span(f"Reducer gets model {i} from S3"):
+                        models.append(self.get_from_s3(model_pred_tuple.model_key))
+                        predictions.append(self.get_from_s3(model_pred_tuple.pred_key))
+        elif self.transferType == XDT:
+            log.info("Reducer gets models from XDT")
+            log.fatal("XDT is not yet supported")
+            # with tracing.Span("Reducer gets models from XDT"):
+            #     for i in range(len(request.model_keys)):
+            #         with tracing.Span(f"Reducer gets model {i} from XDT"):
+            #             models.append(get_from_xdt(request.model_key[i]))
+            #             predictions.append(request.preds[i])
+
+        meta_features = np.transpose(np.array(predictions))
+
+        meta_features_key = 'meta_features'
+        models_key = 'models'
+        if self.transferType == S3:
+            log.info("Reducer puts the meta features to S3")
+            with tracing.Span("Reducer puts the meta features to S3"):
+                self.put_to_s3(meta_features, meta_features_key)
+
+            log.info("Reducer puts the models to S3")
+            with tracing.Span("Reducer puts the models to S3"):
+                self.put_to_s3(models, models_key)
+        elif self.transferType == XDT:
+            log.info("Reducer puts the meta features to XDT")
+            log.fatal("XDT is not yet supported")
+            # with tracing.Span("Reducer puts the meta features to XDT"):
+            #     self.put_to_xdt(meta_features, meta_features_key)
+            #
+            # log.info("Reducer puts the models to XDT")
+            # with tracing.Span("Reducer puts the models to XDT"):
+            #     self.put_to_xdt(models, models_key)
+
+        return stacking_pb2.ReduceReply(
+            models=b'',
+            models_key=models_key,
+            meta_features=b'',
+            meta_features_key=meta_features_key
+        )
+
+
+def serve():
+    transferType = os.getenv('TRANSFER_TYPE', S3)
+    if transferType == S3:
+        log.info("Using inline or s3 transfers")
+        max_workers = int(os.getenv("MAX_SERVER_THREADS", 10))
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=max_workers))
+        stacking_pb2_grpc.add_ReducerServicer_to_server(
+            ReducerServicer(transferType=transferType), server)
+        server.add_insecure_port('[::]:' + args.sp)
+        server.start()
+        server.wait_for_termination()
+    elif transferType == XDT:
+        log.fatal("XDT not yet supported")
+        XDTconfig = XDTutil.loadConfig()
+    else:
+        log.fatal("Invalid Transfer type")
+
+
+if __name__ == '__main__':
+    log.basicConfig(level=log.INFO)
+    serve()
---
+++ function-images/tests/stacking-training/trainer/main.py
@@ -0,0 +1,201 @@
+# MIT License
+#
+# Copyright (c) 2021 EASE lab
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import print_function
+
+import sys
+import os
+import grpc
+import argparse
+import boto3
+import logging as log
+import socket
+
+import sklearn.datasets as datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import LinearSVR
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import roc_auc_score
+import numpy as np
+import pickle
+
+# adding python tracing sources to the system path
+sys.path.insert(0, os.getcwd() + '/../proto/')
+sys.path.insert(0, os.getcwd() + '/../../../../utils/tracing/python')
+import tracing
+import stacking_pb2_grpc
+import stacking_pb2
+import destination as XDTdst
+import source as XDTsrc
+import utils as XDTutil
+
+
+
+from concurrent import futures
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-dockerCompose", "--dockerCompose", dest="dockerCompose", default=False, help="Env docker compose")
+parser.add_argument("-sp", "--sp", dest="sp", default="80", help="serve port")
+parser.add_argument("-zipkin", "--zipkin", dest="zipkinURL",
+                    default="http://zipkin.istio-system.svc.cluster.local:9411/api/v2/spans",
+                    help="Zipkin endpoint url")
+
+args = parser.parse_args()
+
+if tracing.IsTracingEnabled():
+    tracing.initTracer("trainer", url=args.zipkinURL)
+    tracing.grpcInstrumentClient()
+    tracing.grpcInstrumentServer()
+
+INLINE = "INLINE"
+S3 = "S3"
+XDT = "XDT"
+
+# set aws credentials:
+AWS_ID = os.getenv('AWS_ACCESS_KEY', "")
+AWS_SECRET = os.getenv('AWS_SECRET_KEY', "")
+
+
+def get_self_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+
+def model_dispatcher(model_name):
+    if model_name == 'LinearSVR':
+        return LinearSVR
+    elif model_name == 'Lasso':
+        return Lasso
+    elif model_name == 'LinearRegression':
+        return LinearRegression
+    elif model_name == 'RandomForestRegressor':
+        return RandomForestRegressor
+    elif model_name == 'KNeighborsRegressor':
+        return KNeighborsRegressor
+    elif model_name == 'LogisticRegression':
+        return LogisticRegression
+    else:
+        raise ValueError(f"Model {model_name} not found")
+
+
+class TrainerServicer(stacking_pb2_grpc.TrainerServicer):
+    def __init__(self, transferType, XDTconfig=None):
+
+        self.benchName = 'vhive-stacking'
+        self.transferType = transferType
+        self.trainer_id = ""
+        if transferType == S3:
+            self.s3_client = boto3.resource(
+                service_name='s3',
+                region_name=os.getenv("AWS_REGION", 'us-west-1'),
+                aws_access_key_id=AWS_ID,
+                aws_secret_access_key=AWS_SECRET
+            )
+        elif transferType == XDT:
+            if XDTconfig is None:
+                log.fatal("Empty XDT config")
+            self.XDTconfig = XDTconfig
+
+    def put_to_s3(self, obj, key):
+        s3object = self.s3_client.Object(bucket_name=self.benchName, key=key)
+        pickled = pickle.dumps(obj)
+        s3object.put(Body=pickled)
+
+    def get_from_s3(self, key):
+        obj = self.s3_client.Object(bucket_name=self.benchName, key=key)
+        response = obj.get()
+        return pickle.loads(response['Body'].read())
+
+    def Train(self, request, context):
+        self.trainer_id = request.trainer_id
+        log.info(f"Trainer {self.trainer_id} is invoked")
+
+        if self.transferType == S3:
+            log.info("Trainer gets dataset from S3")
+            with tracing.Span("Trainer gets dataset from S3"):
+                dataset = self.get_from_s3(request.dataset_key)
+        elif self.transferType == XDT:
+            log.info("Trainer gets dataset from XDT")
+            log.fatal("XDT is not yet supported")
+
+        with tracing.Span("Training a model"):
+            model_config = pickle.loads(request.model_config)
+
+            # Init model
+            model_class = model_dispatcher(model_config['model'])
+            model = model_class(**model_config['params'])
+
+            # Train model and get predictions
+            y_pred = cross_val_predict(model, dataset['features'], dataset['labels'], cv=5)
+            model.fit(dataset['features'], dataset['labels'])
+            print(f"{model_config['model']} score: {roc_auc_score(dataset['labels'], y_pred)}")
+
+        # Write to S3
+        model_key = f"model_{self.trainer_id}"
+        pred_key = f"pred_model_{self.trainer_id}"
+        if self.transferType == S3:
+            with tracing.Span("Trainer puts a model to S3"):
+                self.put_to_s3(model, model_key)
+            with tracing.Span("Trainer puts a prediction to S3"):
+                self.put_to_s3(y_pred, pred_key)
+        elif self.transferType == XDT:
+            log.fatal("XDT not yet supported")
+
+        return stacking_pb2.TrainReply(
+            model=b'',
+            model_key=model_key,
+            pred_key=pred_key
+        )
+
+
+def serve():
+    transferType = os.getenv('TRANSFER_TYPE', S3)
+    if transferType == S3:
+        log.info("Using inline or s3 transfers")
+        max_workers = int(os.getenv("MAX_SERVER_THREADS", 10))
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=max_workers))
+        stacking_pb2_grpc.add_TrainerServicer_to_server(
+            TrainerServicer(transferType=transferType), server)
+        server.add_insecure_port('[::]:' + args.sp)
+        server.start()
+        server.wait_for_termination()
+    elif transferType == XDT:
+        log.fatal("XDT not yet supported")
+        XDTconfig = XDTutil.loadConfig()
+    else:
+        log.fatal("Invalid Transfer type")
+
+
+if __name__ == '__main__':
+    log.basicConfig(level=log.INFO)
+    serve()
