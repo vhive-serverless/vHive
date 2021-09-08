4eedb49fe5d39f4cf704d841536c22a819c43c21
Franky Romero <faromero@stanford.edu>
2021-09-08 17:23:49 +0000

    Cleanup

--- function-images/minio_scripts/create_object_of_size_and_store_in_bucket.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash                                                                               
-echo -e "Emter desired object size in bytes"
-read objectsize
-truncate -s $objectsize $objectsize.txt
-mc cp $objectsize.txt myminio/mybucket
-rm $objectsize.txt
