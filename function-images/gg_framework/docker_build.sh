e5f87112f934784698fabfa8b45cb6b6b2866929
Franky Romero <faromero@stanford.edu>
2021-09-08 17:27:02 +0000

    License

--- function-images/gg_framework/docker_build.sh
+++ function-images/gg_framework/docker_build.sh
@@ -2,7 +2,7 @@
 
 # MIT License
 # 
-# Copyright (c) 2020 Dmitrii Ustiugov and EASE lab.
+# Copyright (c) 2021 Francisco Romero, Clemente Farias Canepa, and EASE lab.
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
