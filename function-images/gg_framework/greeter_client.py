d89832f17bd3083a08e6241b295d23e25c1a2ae9
Franky Romero <faromero@stanford.edu>
2021-09-08 17:23:26 +0000

    Cleanup

--- function-images/gg_framework/greeter_client.py
+++ function-images/gg_framework/greeter_client.py
@@ -1,32 +1,24 @@
+from __future__ import print_function
 import logging
 
 import grpc
 
 import helloworld_pb2
 import helloworld_pb2_grpc
+
 import json
 import sys
-from minio import Minio
 
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
 
-        minioClient = Minio("10.138.0.27:9000",
-                            access_key="minioadmin",
-                            secret_key="minioadmin",
-                            secure=False)
-
-        input_hash = (minioClient.get_object("mybucket", "hello_world/hello")
-                                .data.decode('utf-8').splitlines())#[1]
-        print(input_hash)
         name = json.dumps({
-            'input_hash': input_hash,
-            'thunks': 'not sure yet',
-            'storageBackend': 'http://10.138.0.27:9000',
+            'input_hash': 'dummy',
+            'thunks': 'dummy',
+            'storageBackend': 'dummy',
             'timelog': False
-            })
-        return 0
+        })
 
         response = stub.SayHello(helloworld_pb2.HelloRequest(name=name))
     print("Greeter client received: " + response.message)
