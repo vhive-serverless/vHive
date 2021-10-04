b0e26071d588a20d17b6bb44ae70d9bcfa3b815f
Dmitrii Ustiugov <dmitrii.ustiugov@ed.ac.uk>
2021-10-04 17:02:52 +0100

    Remove skopeo dependency
    This hotfix breaks the CRI tests. 
    
    Signed-off-by: Dmitrii Ustiugov <dmitrii.ustiugov@ed.ac.uk>

--- scripts/setup_system.sh
+++ scripts/setup_system.sh
@@ -23,9 +23,10 @@
 # SOFTWARE.
 
 # Add skopeo sources
-. /etc/os-release
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list > /dev/null
-curl --silent --show-error -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
+# The code here should be uncommented when https://github.com/ease-lab/vhive/issues/402 is resolved
+# . /etc/os-release
+# echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list > /dev/null
+# curl --silent --show-error -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
 
 sudo apt-get update >> /dev/null
 
@@ -45,8 +46,10 @@ sudo apt-get -y install \
     dmsetup \
     gnupg-agent \
     software-properties-common \
-    iproute2 \
-    skopeo >> /dev/null
+    iproute2 >> /dev/null
+# The code here should be uncommented when https://github.com/ease-lab/vhive/issues/402 is resolved
+#    iproute2 \
+#    skopeo >> /dev/null
 
 # stack size, # of open files, # of pids
 sudo sh -c "echo \"* soft nofile 1000000\" >> /etc/security/limits.conf"
