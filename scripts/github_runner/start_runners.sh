e871dc975b1f3582225b63934e55733b2f9a2d93
Shyam Jesal <s.jesalpura@gmail.com>
2021-10-14 15:57:36 +0530

    update integ test runner to ubuntu 20
    Signed-off-by: Shyam Jesal <s.jesalpura@gmail.com>

--- scripts/github_runner/start_runners.sh
+++ scripts/github_runner/start_runners.sh
@@ -53,7 +53,7 @@ RUNNER_TOKEN="$(curl -XPOST -fsSL \
 case "$RUNNER_LABEL" in
 "integ")
     # pull latest images
-    docker pull vhiveease/integ_test_runner:ubuntu18base
+    docker pull vhiveease/integ_test_runner:ubuntu20base
     docker pull vhiveease/cri_test_runner 
 
     if [ "$RESTART_FLAG" == "restart" ]; then
@@ -74,12 +74,12 @@ case "$RUNNER_LABEL" in
             -v /var/run/docker.sock:/var/run/docker.sock \
             --volume /dev:/dev \
             --volume /run/udev/control:/run/udev/control \
-            vhiveease/integ_test_runner:ubuntu18base
+            vhiveease/integ_test_runner:ubuntu20base
     done
     ;;
 "cri")
     # pull latest images
-    docker pull vhiveease/integ_test_runner:ubuntu18base
+    docker pull vhiveease/integ_test_runner:ubuntu20base
     docker pull vhiveease/cri_test_runner
 
     if [ "$RESTART_FLAG" == "restart" ]; then
