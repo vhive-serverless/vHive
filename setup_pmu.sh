724ff50ac6e7c512eecb7f03bb886d545ba743aa
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2022-02-16 17:33:43 +0100

    Script bugfixing
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- setup_pmu.sh
+++ setup_pmu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scripts/setup_system.sh
+source scripts/setup_system.sh
 
 wget https://go.dev/dl/go1.17.7.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.7.linux-amd64.tar.gz
@@ -10,9 +10,11 @@ sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt update
 sudo apt install git -y
 
-scripts/setup_firecracker_containerd.sh
+source scripts/setup_firecracker_containerd.sh
 go build -race -v -a ./...
 
-scripts/install_pmutools.sh
+source scripts/install_pmutools.sh || true
 
-screen -d -m sudo PATH=$PATH /usr/local/bin/firecracker-containerd --config /etc/firecracker-containerd/config.toml
+screen -d -m -S firecracker bash -c 'sudo PATH=$PATH /usr/local/bin/firecracker-containerd --config /etc/firecracker-containerd/config.toml'
+
+echo "---------------------> DONE"
