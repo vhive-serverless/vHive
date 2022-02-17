6ab2d01bc3b17a18f87922a3e2ef0e9d50002c85
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2022-02-17 10:14:20 +0100

    Update setup_pmu.sh

--- setup_pmu.sh
+++ setup_pmu.sh
@@ -2,7 +2,7 @@
 
 source scripts/setup_system.sh
 
-wget https://go.dev/dl/go1.17.7.linux-amd64.tar.gz
+wget -O go1.17.7.linux-amd64.tar.gz https://go.dev/dl/go1.17.7.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.7.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
@@ -13,8 +13,8 @@ sudo apt install git -y
 source scripts/setup_firecracker_containerd.sh
 go build -race -v -a ./...
 
-source scripts/install_pmutools.sh || true
+#source scripts/install_pmutools.sh || true
 
-screen -d -m -S firecracker bash -c 'sudo PATH=$PATH /usr/local/bin/firecracker-containerd --config /etc/firecracker-containerd/config.toml'
+#screen -d -m -S firecracker bash -c 'sudo PATH=$PATH /usr/local/bin/firecracker-containerd --config /etc/firecracker-containerd/config.toml'
 
-echo "---------------------> DONE"
+#echo "---------------------> DONE"
