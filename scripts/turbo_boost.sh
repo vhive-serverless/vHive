a3247e1a3ab3235386a0b69335efe52755baa624
Hongyu <hongyuhe.cs@googlemail.com>
2022-05-13 17:52:52 +0200

    Add turbo boost setup
    Signed-off-by: Hongyu <hongyuhe.cs@googlemail.com>

---
+++ scripts/turbo_boost.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if [[ -z $(which rdmsr) ]]; then
+    sudo apt-get install msr-tools
+fi
+
+if [[ ! -z $1 && $1 != "enable" && $1 != "disable" ]]; then
+    echo "Invalid argument: $1" >&2
+    echo ""
+    echo "Usage: $(basename $0) [disable|enable]"
+    exit 1
+fi
+
+#* Necessary!
+sudo modprobe msr
+
+cores=$(cat /proc/cpuinfo | grep processor | awk '{print $3}')
+for core in $cores; do
+    if [[ $1 == "disable" ]]; then
+        sudo wrmsr -p${core} 0x1a0 0x4000850089
+    fi
+    if [[ $1 == "enable" ]]; then
+        sudo wrmsr -p${core} 0x1a0 0x850089
+    fi
+    state=$(sudo rdmsr -p${core} 0x1a0 -f 38:38)
+    if [[ $state -eq 1 ]]; then
+        echo "core ${core}: disabled"
+    else
+        echo "core ${core}: enabled"
+    fi
+done
