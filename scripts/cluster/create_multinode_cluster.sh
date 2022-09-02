725165db9e48ef75c7a8f2670ca4d052a583f54b
Dmitrii Ustiugov <dmitrii.ustiugov@ed.ac.uk>
2022-09-02 15:46:59 +0200

    configure pod network cidr range
    Signed-off-by: Dmitrii Ustiugov <dmitrii.ustiugov@ed.ac.uk>

--- scripts/cluster/create_multinode_cluster.sh
+++ scripts/cluster/create_multinode_cluster.sh
@@ -34,7 +34,7 @@ Environment="KUBELET_EXTRA_ARGS=--container-runtime=remote --runtime-request-tim
 EOF'
 sudo systemctl daemon-reload
 
-sudo kubeadm init --ignore-preflight-errors=all --cri-socket /run/containerd/containerd.sock --pod-network-cidr=192.168.0.0/16
+sudo kubeadm init --v=7 --apiserver-advertise-address=10.0.1.1 --cri-socket /run/containerd/containerd.sock --pod-network-cidr=10.168.0.0/16
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
