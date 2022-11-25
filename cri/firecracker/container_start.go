08caff30c1c2d1e52d553b0bfae50b03f4137af7
Diego Meyer <diego.a.meyer@gmail.com>
2022-11-25 14:18:21 +0100

    Check if LoadContainer works as expected

--- cri/firecracker/container_start.go
+++ cri/firecracker/container_start.go
@@ -2,12 +2,14 @@ package firecracker
 
 import (
 	"context"
+	"github.com/containerd/containerd/namespaces"
 	log "github.com/sirupsen/logrus"
 	criapi "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
 )
 
 // StartContainer starts the container.
 func (fs *FirecrackerService) StartContainer(ctx context.Context, r *criapi.StartContainerRequest) (retRes *criapi.StartContainerResponse, retErr error) {
+	ctx = namespaces.WithNamespace(ctx, "k8s.io")
 	containerId := r.GetContainerId()
 
 	cntr, err := fs.firecrackerContainerdClient.LoadContainer(ctx, containerId)
