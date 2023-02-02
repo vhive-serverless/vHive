97dea90bd23809cb7948e317c85488f658d23a9b
Estella Negoita <paulanegoita@yahoo.com>
2023-02-02 22:29:35 +0100

    Modify benchmarking script for function creation and invocation

--- scripts/master_script.sh
+++ scripts/master_script.sh
@@ -4,17 +4,20 @@
 rm -rf log_master_script.txt
 touch log_master_script.txt
 
+for i in {1..1}
+do
 
-now=$(date +"%T")
-echo "Invoke function run i START at time: $now" >> log_master_script.txt
+	now=$(date +"%T")
+	echo "Invoke function run $i START at time: $now" >> log_master_script.txt
 
-source /etc/profile && pushd ./examples/deployer && go build && popd && ./examples/deployer/deployer
+	source /etc/profile && pushd ./examples/deployer && go build && popd && ./examples/deployer/deployer
 
-now=$(date +"%T")
-echo "Invoke function run i END at time: $now" >> log_master_script.txt
+	now=$(date +"%T")
+	echo "Invoke function run $i END at time: $now" >> log_master_script.txt
 
-echo "Sleep for 1 minute" >> log_master_script.txt
-sleep 60
+	echo "Sleep for 1 minute" >> log_master_script.txt
+	sleep 60
 
-now=$(date +"%T")
-echo "Sleep ended at time: $now" >> log_master_script.txt
+	now=$(date +"%T")
+	echo "Sleep ended at time run $i: $now" >> log_master_script.txt
+done
