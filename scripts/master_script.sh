81b7e5a5322f003b3a864a60a391193ca353a04a
Estella Negoita <paulanegoita@yahoo.com>
2023-02-03 06:37:58 +0100

    Modify benchmarking script for function creation and invocation

--- scripts/master_script.sh
+++ scripts/master_script.sh
@@ -2,22 +2,35 @@
 
 # create log file
 rm -rf log_master_script.txt
+rm -rf ./latencies
+mkdir latencies
 touch log_master_script.txt
 
-for i in {1..1}
+echo -e "Deploy function\n"
+source /etc/profile >> log_master_script.txt && pushd ./examples/deployer >> log_master_script.txt && go build >> log_master_script.txt && popd && ./examples/deployer/deployer >> log_master_script.txt
+echo -e "Deploy function ended\n"
+
+
+for i in {1..200}
 do
 
-	now=$(date +"%T")
-	echo "Invoke function run $i START at time: $now" >> log_master_script.txt
+	ts=$(date +%s%N)
+	echo -e "Invoke function run $i START\n" >> log_master_script.txt
 
-	source /etc/profile && pushd ./examples/deployer && go build && popd && ./examples/deployer/deployer
+	pushd ./examples/invoker >> log_master_script.txt && go build >> log_master_script.txt && popd && ./examples/invoker/invoker >> log_master_script.txt
 
-	now=$(date +"%T")
-	echo "Invoke function run $i END at time: $now" >> log_master_script.txt
 
-	echo "Sleep for 1 minute" >> log_master_script.txt
-	sleep 60
+	echo -e "Invoke function run $i END at time: $((($(date +%s%N) - $ts)/1000000))\n" >> log_master_script.txt
 
 	now=$(date +"%T")
-	echo "Sleep ended at time run $i: $now" >> log_master_script.txt
+	echo -e "Sleep for 1 minute: $now\n" >> log_master_script.txt
+	sleep 70
+
+	now=$(date +"%T")
+	echo -e "Sleep ended at time run $i: $now\n" >> log_master_script.txt
+
+	touch ./latencies/rps$i.00_lat.csv
+	cat rps1.00_lat.csv >> ./latencies/rps$i.00_lat.csv
+	echo -e "Check if pods still up\n" >> log_master_script.txt
+	kubectl get pods -A | head -n 5 >> log_master_script.txt
 done
