4667f8fec0e0f2f5b6fc7794fd5204abc1ffadf6
Estella Negoita <paulanegoita@yahoo.com>
2023-02-11 17:13:18 +0100

    Add benchmark script for function image pull.

---
+++ scripts/bench_images.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# create log file
+rm -rf log_image_pulls.txt
+touch log_image_pulls.txt
+
+sudo docker image rm ghcr.io/ease-lab/helloworld:var_workload
+sudo docker image rm ghcr.io/ease-lab/pyaes:var_workload
+sudo docker image rm vhiveease/rnn_serving:var_workload
+
+for i in {1..100}
+do
+	echo -e "helloworld pull  $i: \n" >> log_master_script.txt
+	time sudo docker pull ghcr.io/ease-lab/helloworld:var_workload
+	echo -e "pyaes pull  $i: \n" >> log_master_script.txt
+	time sudo docker pull ghcr.io/ease-lab/pyaes:var_workload
+  echo -e "rnn pull  $i: \n" >> log_master_script.txt
+	time sudo docker pull vhiveease/rnn_serving:var_workload
+
+	# remove images
+	sudo docker image rm ghcr.io/ease-lab/helloworld:var_workload
+	sudo docker image rm ghcr.io/ease-lab/pyaes:var_workload
+	sudo docker image rm vhiveease/rnn_serving:var_workload
+done
