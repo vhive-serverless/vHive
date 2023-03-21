62b8f0281126a5b654ef8571adc95b2383c53b62
Anshal Shukla <shukla.anshal85@gmail.com>
2023-03-21 21:27:27 +0530

    Update metallb version and calico config
    Signed-off-by: Anshal Shukla <shukla.anshal85@gmail.com>

--- scripts/cluster/setup_master_node.sh
+++ scripts/cluster/setup_master_node.sh
@@ -36,10 +36,10 @@ kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl apply -f - -n kube-system
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12/manifests/metallb.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f $ROOT/configs/metallb/metallb-configmap.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.9/config/manifests/metallb-native.yaml
+kubectl -n metallb-system wait deploy controller --timeout=90s --for=condition=Available
+kubectl apply -f $ROOT/configs/metallb/metallb-ipaddresspool.yaml
+kubectl apply -f $ROOT/configs/metallb/metallb-l2advertisement.yaml
 
 # istio
 cd $ROOT
