0be07def8448cd657a2a5697029a58f82f303085
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2023-03-22 13:47:27 +0100

    Fix metalLB
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- scripts/cluster/setup_master_node.sh
+++ scripts/cluster/setup_master_node.sh
@@ -38,8 +38,8 @@ kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl apply -f - -n kube-system
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f $ROOT/configs/metallb/metallb-configmap.yaml
 
