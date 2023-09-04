e940948e3d5bced86bb4ef4fe3ea7cc381eaf1db
Georgiy Lebedev <lebedev.gk@phystech.edu>
2023-09-04 12:53:15 +0300

    New network topology for firecracker VMs
    Currently, each firecracker VM needs to use a TAP network device, to route
    its packages into the network stack of the physical host. When saving and
    restoring a function instance, the tap device name and the IP address of
    the functions’ server, running inside the container, are preserved (see
    also the current requirements for vanilla firecracker snapshot
    loading [1]). This leads to networking conflicts on the host and limits the
    snapshot restoration to a single instance per physical machine.
    
    To bypass this obstacle, the following network topology is proposed:
    
    1. A new network namespace (e.g.: VMns4) is created for each VM, in which
    the TAP device from the snapshotted VM is rebuilt and receives the original
    IP address of the function. The TAP device will broadcast all the incoming
    and outgoing packets to and from the serverless function and VM’s network
    interface. Each VM will run in its own network namespace, leading to no
    conflicts on the host due to networking resources.
    
    2. A local virtual tunnel is established between the VM inside its network
    namespace and the host node via a virtual ethernet pair (veth). A link is
    then established between the two ends of the virtual ethernet pair, in the
    network namespace (veth4-0) and the host namespace (veth4-1). In contrast,
    the default vHive configuration sets up a similar forwarding system through
    network bridges.
    
    3. Inside the network namespace we add a routing rule that redirects all
    packets via the veth VM end towards a default gateway (172.17.0.17). Thus,
    all packets sent by the function will show at the hosts’ end of the tunnel.
    
    4. To avoid IP conflicts when routing the packets to and from functions,
    each VM is assigned a unique clone address (172.18.0.5). All packets
    leaving the VM end of the virtual ethernet pair get their source address
    rewritten to the clone address of the corresponding VM. Packets entering
    the host end of the virtual ethernet pair get their destination address
    written to the original address of the VM. As a result, each VM still
    thinks it is using the original address while in reality, its address is
    translated to a clone address, different for every VM. This is accomplished
    using two rules in the NAT table corresponding to the virtual namespace of
    the VM. One rule is added in the POSTROUTING chain and one in the
    PREROUTING chain. The POSTROUTING rule alters the network packets before
    they are sent out in the virtual tunnel, from the VM namespace to the host,
    and rewrites the IP source address of the packet. Similarly, the PREROUTING
    rule overwrites the destination address of incoming packets, before
    routing. The two ensure that packets going into the virtual namespace have
    their destination address the original IP address of the VM (172.16.0.2),
    while packets coming out of the namespace have their source address the
    clone IP address (172.18.05). The source IP address will remain the same
    for all the VM in the enhanced snapshotting mode, being set to 172.16.0.2
    respectively.
    
    5. In the routing table of the host, we add a rule that dictates that any
    package that has as destination IP the clone IP of a VM, will be routed
    towards the end of the tunnel situated in the corresponding network
    namespace, through a set gateway (172.17.0.18). This ensures that whenever
    packages arrive on the host for a VM, they will be sent down the right
    virtual tunnel instantaneously.
    
    6. In the hosts NFT filter table we add 2 rules for the FORWARD chain, that
    allow traffic from the host end of the veth pair (veth4-1) to the default
    host interface (eno 49) and vice versa.
    
    Introduce a new networking management component for the topology described
    above.
    
    1. https://github.com/firecracker-microvm/firecracker/blob/main/docs/snapshotting/snapshot-support.md#loading-snapshots
    
    Closes #797
    Part of #794
    
    Signed-off-by: Georgiy Lebedev <lebedev.gk@phystech.edu>

---
+++ networking/networkManager.go
@@ -0,0 +1,253 @@
+// MIT License
+//
+// Copyright (c) 2023 Georgiy Lebedev, Amory Hoste and vHive team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Package networking provides primitives to connect function instances to the network.
+package networking
+
+import (
+	log "github.com/sirupsen/logrus"
+	"sync"
+)
+
+// NetworkManager manages the in use network configurations along with a pool of free network configurations
+// that can be used to connect a function instance to the network.
+type NetworkManager struct {
+	sync.Mutex
+	nextID        int
+	hostIfaceName string
+
+	// Pool of free network configs
+	networkPool []*NetworkConfig
+	poolCond    *sync.Cond
+	poolSize    int
+
+	// Mapping of function instance IDs to their network config
+	netConfigs map[string]*NetworkConfig
+
+	// Network configs that are being created
+	inCreation sync.WaitGroup
+}
+
+// NewNetworkManager creates and returns a new network manager that connects function instances to the network
+// using the supplied interface. If no interface is supplied, the default interface is used. To take the network
+// setup of the critical path of a function creation, the network manager tries to maintain a pool of ready to use
+// network configurations of size at least poolSize.
+func NewNetworkManager(hostIfaceName string, poolSize int) (*NetworkManager, error) {
+	manager := new(NetworkManager)
+
+	manager.hostIfaceName = hostIfaceName
+	if manager.hostIfaceName == "" {
+		hostIface, err := getHostIfaceName()
+		if err != nil {
+			return nil, err
+		} else {
+			manager.hostIfaceName = hostIface
+		}
+	}
+
+	manager.netConfigs = make(map[string]*NetworkConfig)
+	manager.networkPool = make([]*NetworkConfig, 0)
+
+	startId, err := getNetworkStartID()
+	if err == nil {
+		manager.nextID = startId
+	} else {
+		manager.nextID = 0
+	}
+
+	manager.poolCond = sync.NewCond(new(sync.Mutex))
+	manager.initConfigPool(poolSize)
+	manager.poolSize = poolSize
+
+	return manager, nil
+}
+
+// initConfigPool fills an empty network pool up to the given poolSize
+func (mgr *NetworkManager) initConfigPool(poolSize int) {
+	var wg sync.WaitGroup
+	wg.Add(poolSize)
+
+	logger := log.WithFields(log.Fields{"poolSize": poolSize})
+	logger.Debug("Initializing network pool")
+
+	// Concurrently create poolSize network configs
+	for i := 0; i < poolSize; i++ {
+		go func() {
+			mgr.addNetConfig()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+// addNetConfig creates and initializes a new network config
+func (mgr *NetworkManager) addNetConfig() {
+	mgr.Lock()
+	id := mgr.nextID
+	mgr.nextID += 1
+	mgr.inCreation.Add(1)
+	mgr.Unlock()
+
+	netCfg := NewNetworkConfig(id, mgr.hostIfaceName)
+	if err := netCfg.CreateNetwork(); err != nil {
+		log.Errorf("failed to create network %s:", err)
+	}
+
+	mgr.poolCond.L.Lock()
+	mgr.networkPool = append(mgr.networkPool, netCfg)
+	// Signal in case someone is waiting for a new config to become available in the pool
+	mgr.poolCond.Signal()
+	mgr.poolCond.L.Unlock()
+	mgr.inCreation.Done()
+}
+
+// allocNetConfig allocates a new network config from the pool to a function instance identified by funcID
+func (mgr *NetworkManager) allocNetConfig(funcID string) *NetworkConfig {
+	// Add netconfig to pool to keep pool to configured size
+	go mgr.addNetConfig()
+
+	logger := log.WithFields(log.Fields{"funcID": funcID})
+	logger.Debug("Allocating a new network config from network pool to function instance")
+
+	// Pop a network config from the pool and allocate it to the function instance
+	mgr.poolCond.L.Lock()
+	for len(mgr.networkPool) == 0 {
+		// Wait until a new network config has been created
+		mgr.poolCond.Wait()
+	}
+
+	config := mgr.networkPool[len(mgr.networkPool)-1]
+	mgr.networkPool = mgr.networkPool[:len(mgr.networkPool)-1]
+	mgr.poolCond.L.Unlock()
+
+	mgr.Lock()
+	mgr.netConfigs[funcID] = config
+	mgr.Unlock()
+
+	logger = log.WithFields(log.Fields{
+		"funcID":        funcID,
+		"ContainerIP":   config.getContainerIP(),
+		"NamespaceName": config.getNamespaceName(),
+		"Veth0CIDR":     config.getVeth0CIDR(),
+		"Veth0Name":     config.getVeth0Name(),
+		"Veth1CIDR":     config.getVeth1CIDR(),
+		"Veth1Name":     config.getVeth1Name(),
+		"CloneIP":       config.GetCloneIP(),
+		"ContainerCIDR": config.GetContainerCIDR(),
+		"GatewayIP":     config.GetGatewayIP(),
+		"HostDevName":   config.GetHostDevName(),
+		"NamespacePath": config.GetNamespacePath()})
+
+	logger.Debug("Allocated a new network config")
+
+	return config
+}
+
+// releaseNetConfig releases the network config of a given function instance with id funcID back to the pool
+func (mgr *NetworkManager) releaseNetConfig(funcID string) {
+	mgr.Lock()
+	config := mgr.netConfigs[funcID]
+	delete(mgr.netConfigs, funcID)
+	mgr.Unlock()
+
+	logger := log.WithFields(log.Fields{"funcID": funcID})
+	logger.Debug("Releasing network config from function instance and adding it to network pool")
+
+	// Add network config back to the pool. We allow the pool to grow over it's configured size here since the
+	// overhead of keeping a network config in the pool is low compared to the cost of creating a new config.
+	mgr.poolCond.L.Lock()
+	mgr.networkPool = append(mgr.networkPool, config)
+	mgr.poolCond.Signal()
+	mgr.poolCond.L.Unlock()
+}
+
+// CreateNetwork creates the networking for a function instance identified by funcID
+func (mgr *NetworkManager) CreateNetwork(funcID string) (*NetworkConfig, error) {
+	logger := log.WithFields(log.Fields{"funcID": funcID})
+	logger.Debug("Creating network config for function instance")
+
+	netCfg := mgr.allocNetConfig(funcID)
+	return netCfg, nil
+}
+
+// GetConfig returns the network config assigned to a function instance identified by funcID
+func (mgr *NetworkManager) GetConfig(funcID string) *NetworkConfig {
+	mgr.Lock()
+	defer mgr.Unlock()
+
+	cfg := mgr.netConfigs[funcID]
+	return cfg
+}
+
+// RemoveNetwork removes the network config of a function instance identified by funcID. The allocated network devices
+// for the given function instance must not be in use anymore when calling this function.
+func (mgr *NetworkManager) RemoveNetwork(funcID string) error {
+	logger := log.WithFields(log.Fields{"funcID": funcID})
+	logger.Debug("Removing network config for function instance")
+	mgr.releaseNetConfig(funcID)
+	return nil
+}
+
+// Cleanup removes and deallocates all network configurations that are in use or in the network pool. Make sure to first
+// clean up all running functions before removing their network configs.
+func (mgr *NetworkManager) Cleanup() error {
+	log.Info("Cleaning up network manager")
+	mgr.Lock()
+	defer mgr.Unlock()
+
+	// Wait till all network configs still in creation are added
+	mgr.inCreation.Wait()
+
+	// Release network configs still in use
+	var wgu sync.WaitGroup
+	wgu.Add(len(mgr.netConfigs))
+	for funcID := range mgr.netConfigs {
+		config := mgr.netConfigs[funcID]
+		go func(config *NetworkConfig) {
+			if err := config.RemoveNetwork(); err != nil {
+				log.Errorf("failed to remove network %s:", err)
+			}
+			wgu.Done()
+		}(config)
+	}
+	wgu.Wait()
+	mgr.netConfigs = make(map[string]*NetworkConfig)
+
+	// Cleanup network pool
+	mgr.poolCond.L.Lock()
+	var wg sync.WaitGroup
+	wg.Add(len(mgr.networkPool))
+
+	for _, config := range mgr.networkPool {
+		go func(config *NetworkConfig) {
+			if err := config.RemoveNetwork(); err != nil {
+				log.Errorf("failed to remove network %s:", err)
+			}
+			wg.Done()
+		}(config)
+	}
+	wg.Wait()
+	mgr.networkPool = make([]*NetworkConfig, 0)
+	mgr.poolCond.L.Unlock()
+
+	return nil
+}
---
+++ networking/networkconfig.go
@@ -0,0 +1,292 @@
+// MIT License
+//
+// Copyright (c) 2023 Georgiy Lebedev, Amory Hoste and vHive team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package networking
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	"github.com/vishvananda/netns"
+	"net"
+	"runtime"
+)
+
+const (
+	defaultContainerCIDR = "172.16.0.2/24"
+	defaultGatewayCIDR   = "172.16.0.1/24"
+	defaultContainerTap  = "tap0"
+	defaultContainerMac  = "AA:FC:00:00:00:01"
+)
+
+// NetworkConfig represents the network devices, IPs, namespaces, routes and filter rules to connect a uVM
+// to the network. The network config ID is deterministically mapped to IP addresses to be used for the uVM.
+// Note that due to the current allocation of IPs at most 2^14 VMs can be simultaneously be available on a single host.
+type NetworkConfig struct {
+	id            int
+	containerCIDR string // Container IP address (CIDR notation)
+	gatewayCIDR   string // Container gateway IP address (CIDR notation)
+	containerTap  string // Container tap name
+	containerMac  string // Container Mac address
+	hostIfaceName string // Host network interface name
+}
+
+// NewNetworkConfig creates a new network config with a given id and default host interface
+func NewNetworkConfig(id int, hostIfaceName string) *NetworkConfig {
+	return &NetworkConfig{
+		id:            id,
+		containerCIDR: defaultContainerCIDR,
+		gatewayCIDR:   defaultGatewayCIDR,
+		containerTap:  defaultContainerTap,
+		containerMac:  defaultContainerMac,
+		hostIfaceName: hostIfaceName,
+	}
+}
+
+// GetMacAddress returns the mac address used for the uVM
+func (cfg *NetworkConfig) GetMacAddress() string {
+	return cfg.containerMac
+}
+
+// GetHostDevName returns the device connecting the uVM to the host
+func (cfg *NetworkConfig) GetHostDevName() string {
+	return cfg.containerTap
+}
+
+// getVeth0Name returns the name for the veth device at the side of the uVM
+func (cfg *NetworkConfig) getVeth0Name() string {
+	return fmt.Sprintf("veth%d-0", cfg.id)
+}
+
+// getVeth0CIDR returns the IP address for the veth device at the side of the uVM in CIDR notation
+func (cfg *NetworkConfig) getVeth0CIDR() string {
+	return fmt.Sprintf("172.17.%d.%d/30", (4*cfg.id)/256, ((4*cfg.id)+2)%256)
+}
+
+// getVeth1Name returns the name for the veth device at the side of the host
+func (cfg *NetworkConfig) getVeth1Name() string {
+	return fmt.Sprintf("veth%d-1", cfg.id)
+}
+
+// getVeth1Name returns the IP address for the veth device at the side of the host in CIDR notation
+func (cfg *NetworkConfig) getVeth1CIDR() string {
+	return fmt.Sprintf("172.17.%d.%d/30", (4*cfg.id)/256, ((4*cfg.id)+1)%256)
+}
+
+// GetCloneIP returns the IP address the uVM is reachable at from the host
+func (cfg *NetworkConfig) GetCloneIP() string {
+	return fmt.Sprintf("172.18.%d.%d", cfg.id/254, 1+(cfg.id%254))
+}
+
+// GetContainerCIDR returns the internal IP of the uVM in CIDR notation
+func (cfg *NetworkConfig) GetContainerCIDR() string {
+	return cfg.containerCIDR
+}
+
+// getNamespaceName returns the network namespace name for the uVM
+func (cfg *NetworkConfig) getNamespaceName() string {
+	return fmt.Sprintf("uvmns%d", cfg.id)
+}
+
+// GetNamespacePath returns the full path to the network namespace for the uVM
+func (cfg *NetworkConfig) GetNamespacePath() string {
+	return fmt.Sprintf("/var/run/netns/%s", cfg.getNamespaceName())
+}
+
+// getContainerIP returns the internal IP of the uVM
+func (cfg *NetworkConfig) getContainerIP() string {
+	ip, _, _ := net.ParseCIDR(cfg.containerCIDR)
+	return ip.String()
+}
+
+// GetGatewayIP returns the IP address of the tap device associated with the uVM
+func (cfg *NetworkConfig) GetGatewayIP() string {
+	ip, _, _ := net.ParseCIDR(cfg.gatewayCIDR)
+	return ip.String()
+}
+
+// createVmNetwork creates network devices, namespaces, routes and filter rules for the uVM at the
+// uVM side
+func (cfg *NetworkConfig) createVmNetwork(hostNsHandle netns.NsHandle) error {
+	// A. In uVM netns
+	// A.1. Create network namespace for uVM & join network namespace
+	vmNsHandle, err := netns.NewNamed(cfg.getNamespaceName()) // Switches namespace
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer func() { _ = vmNsHandle.Close() }()
+
+	// A.2. Create tap device for uVM
+	if err := createTap(cfg.containerTap, cfg.gatewayCIDR, cfg.getNamespaceName()); err != nil {
+		return err
+	}
+
+	// A.3. Create veth pair for uVM
+	// A.3.1 Create veth pair
+	if err := createVethPair(cfg.getVeth0Name(), cfg.getVeth1Name(), vmNsHandle, hostNsHandle); err != nil {
+		return err
+	}
+
+	// A.3.2 Configure uVM side veth pair
+	if err := configVeth(cfg.getVeth0Name(), cfg.getVeth0CIDR()); err != nil {
+		return err
+	}
+
+	// A.3.3 Designate host side as default gateway for packets leaving namespace
+	if err := setDefaultGateway(cfg.getVeth1CIDR()); err != nil {
+		return err
+	}
+
+	// A.4. Setup NAT rules
+	if err := setupNatRules(cfg.getVeth0Name(), cfg.getContainerIP(), cfg.GetCloneIP(), vmNsHandle); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// createHostNetwork creates network devices, namespaces, routes and filter rules for the uVM at the host
+// side
+func (cfg *NetworkConfig) createHostNetwork() error {
+	// B. In host netns
+	// B.1 Configure host side veth pair
+	if err := configVeth(cfg.getVeth1Name(), cfg.getVeth1CIDR()); err != nil {
+		return err
+	}
+
+	// B.2 Add a route on the host for the clone address
+	if err := addRoute(cfg.GetCloneIP(), cfg.getVeth0CIDR()); err != nil {
+		return err
+	}
+
+	// B.3 Setup nat to route traffic out of veth device
+	if err := setupForwardRules(cfg.getVeth1Name(), cfg.hostIfaceName); err != nil {
+		return err
+	}
+	return nil
+}
+
+// CreateNetwork creates the necessary network devices, namespaces, routes and filter rules to connect the uVM to the
+// network. The networking is created as described in the Firecracker documentation on providing networking for clones
+// (https://github.com/firecracker-microvm/firecracker/blob/main/docs/snapshotting/network-for-clones.md)
+func (cfg *NetworkConfig) CreateNetwork() error {
+	// 1. Lock the OS Thread so we don't accidentally switch namespaces
+	runtime.LockOSThread()
+
+	// 2. Get host network namespace
+	hostNsHandle, err := netns.Get()
+	defer func() { _ = hostNsHandle.Close() }()
+	if err != nil {
+		log.Printf("Failed to get host ns, %s\n", err)
+		return err
+	}
+
+	// 3. Setup networking in instance namespace
+	if err := cfg.createVmNetwork(hostNsHandle); err != nil {
+		_ = netns.Set(hostNsHandle)
+		runtime.UnlockOSThread()
+		return err
+	}
+
+	// 4. Go back to host namespace
+	err = netns.Set(hostNsHandle)
+	if err != nil {
+		return err
+	}
+
+	runtime.UnlockOSThread()
+
+	// 5. Setup networking in host namespace
+	if err := cfg.createHostNetwork(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// CreateNetwork removes the necessary network devices, namespaces, routes and filter rules to connect the
+// function instance to the network
+func (cfg *NetworkConfig) RemoveNetwork() error {
+	// Delete nat to route traffic out of veth device
+	if err := deleteForwardRules(cfg.getVeth1Name()); err != nil {
+		return err
+	}
+
+	// Delete route on the host for the clone address
+	if err := deleteRoute(cfg.GetCloneIP(), cfg.getVeth0CIDR()); err != nil {
+		return err
+	}
+
+	runtime.LockOSThread()
+
+	hostNsHandle, err := netns.Get()
+	defer func() { _ = hostNsHandle.Close() }()
+	if err != nil {
+		log.Printf("Failed to get host ns, %s\n", err)
+		return err
+	}
+
+	// Get uVM namespace handle
+	vmNsHandle, err := netns.GetFromName(cfg.getNamespaceName())
+	defer func() { _ = vmNsHandle.Close() }()
+	if err != nil {
+		return err
+	}
+	err = netns.Set(vmNsHandle)
+	if err != nil {
+		return err
+	}
+
+	// Delete NAT rules
+	if err := deleteNatRules(vmNsHandle); err != nil {
+		return err
+	}
+
+	// Delete default gateway for packets leaving namespace
+	if err := deleteDefaultGateway(cfg.getVeth1CIDR()); err != nil {
+		return err
+	}
+
+	// Delete uVM side veth pair
+	if err := deleteVethPair(cfg.getVeth0Name(), cfg.getVeth1Name(), vmNsHandle, hostNsHandle); err != nil {
+		return err
+	}
+
+	// Delete tap device for uVM
+	if err := deleteTap(cfg.containerTap); err != nil {
+		return err
+	}
+
+	// Delete namespace
+	if err := netns.DeleteNamed(cfg.getNamespaceName()); err != nil {
+		return errors.Wrapf(err, "deleting network namespace")
+	}
+
+	err = netns.Set(hostNsHandle)
+	if err != nil {
+		return err
+	}
+	runtime.UnlockOSThread()
+
+	return nil
+}
