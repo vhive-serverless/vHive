bcdabb3e75a67158016a5468d0be7576fd20c64e
sokunthea.eang <sokunthea.eang@bytedance.com>
2023-10-29 20:08:52 +0800

    test

--- examples/powermanger/client.go
+++ examples/powermanger/client.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os/exec"
 	"time"
-
-	"github.com/prometheus/common/model"
 )
 
 const (
@@ -34,60 +29,30 @@ func setCPUFrequency(frequency int) error {
 	return nil
 }
 
-func fetchPrometheusMetric(prometheusURL, prometheusQuery string) (float64, error) {
-	url := fmt.Sprintf("%s?query=%s", prometheusURL, prometheusQuery)
-	resp, err := http.Get(url)
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return 0, fmt.Errorf("Prometheus request failed with status: %s", resp.Status)
-	}
-
-	// Read and parse the response body.
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return 0, err
-	}
-
-	// Parse the Prometheus response.
-	result := model.Vector{}
-	if err := json.Unmarshal(body, &result); err != nil {
-		return 0, err
-	}
-
-	// want the second result of the query.
-	if len(result) > 1 {
-		return float64(result[1].Value), nil
-	}
-
-	return 0, fmt.Errorf("No data found in Prometheus response")
-}
-
 func main() {
 	// Define your Prometheus query and threshold values
-	prometheusURL := "http://127.0.0.1:9090/api/v1"
-	prometheusQuery := "(avg by(instance) (rate(node_cpu_seconds_total{mode=\"idle\"}[2m])) * 100)"
+	command := "curl -G 'http://127.0.0.1:9090/api/v1/query?' --data-urlencode 'query=(avg by(instance) (rate(node_cpu_seconds_total{mode=\"idle\"}[2m])) * 100)' | jq -r '.data.result[1].value[1]'\n"
 	thresholdHigh := 80.0 // Mostly idle => decrease frequency
 	thresholdLow := 20.0  // Mostly CPU bound => increase frequency
 
 	for {
-		metricValue, err := fetchPrometheusMetric(prometheusURL, prometheusQuery)
+		cmd := exec.Command("bash", "-c", command)
+
+		output, err := cmd.CombinedOutput()
 		if err != nil {
 			fmt.Printf(fmt.Sprintf("ERR :%+v", err))
 		}
+		fmt.Println(output)
 
-		if metricValue > thresholdHigh {
-			if err := setCPUFrequency(Low); err != nil {
-				fmt.Println("Failed to set low CPU frequency:", err)
-			}
-		} else if metricValue != 0 && metricValue < thresholdLow {
-			if err := setCPUFrequency(High); err != nil {
-				fmt.Println("Failed to set high CPU frequency:", err)
-			}
-		}
+		//if metricValue > thresholdHigh {
+		//	if err := setCPUFrequency(Low); err != nil {
+		//		fmt.Println("Failed to set low CPU frequency:", err)
+		//	}
+		//} else if metricValue != 0 && metricValue < thresholdLow {
+		//	if err := setCPUFrequency(High); err != nil {
+		//		fmt.Println("Failed to set high CPU frequency:", err)
+		//	}
+		//}
 
 		time.Sleep(60 * time.Second) // Adjust the polling interval as needed
 	}
