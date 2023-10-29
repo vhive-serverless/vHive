ef31ce75ea2b08921d4321e3f000eeb44e9c8112
sokunthea.eang <sokunthea.eang@bytedance.com>
2023-10-29 20:09:31 +0800

    test

--- examples/powermanger/client.go
+++ examples/powermanger/client.go
@@ -32,8 +32,8 @@ func setCPUFrequency(frequency int) error {
 func main() {
 	// Define your Prometheus query and threshold values
 	command := "curl -G 'http://127.0.0.1:9090/api/v1/query?' --data-urlencode 'query=(avg by(instance) (rate(node_cpu_seconds_total{mode=\"idle\"}[2m])) * 100)' | jq -r '.data.result[1].value[1]'\n"
-	thresholdHigh := 80.0 // Mostly idle => decrease frequency
-	thresholdLow := 20.0  // Mostly CPU bound => increase frequency
+	//thresholdHigh := 80.0 // Mostly idle => decrease frequency
+	//thresholdLow := 20.0  // Mostly CPU bound => increase frequency
 
 	for {
 		cmd := exec.Command("bash", "-c", command)
