7eb1cdcc1d53c89748b45234439e78885908eedb
HermioneKT <hermionegrangerkt@gmail.com>
2024-03-06 00:14:18 +0800

    test

--- power_manager/internode_scaling_exp.go
+++ power_manager/internode_scaling_exp.go
@@ -91,7 +91,7 @@ func processLatencies(records []int64, serviceName string) {
 	ninetiethPercentile := percentile(records, 95)
 	difference := float64(ninetiethPercentile-fifthPercentile) / float64(fifthPercentile)
 	fmt.Println(serviceName, difference)
-	if difference > 0.40 && !ServiceAssignment[serviceName]  {  // Assign to high performance class
+	if difference > 0.30 && !ServiceAssignment[serviceName] { // Assign to high performance class
 		fmt.Println("Assigning to high performance class")
 		command := fmt.Sprintf("kubectl patch service.serving.knative.dev %s --type merge --patch '{\"spec\":{\"template\":{\"spec\":{\"nodeSelector\":{\"loader-nodetype\":\"worker-high\"}}}}}' --namespace default", serviceName)
 		cmd := exec.Command("bash", "-c", command)
