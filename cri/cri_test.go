a168c108dc37c2a2835cf00f9958b66abd1672d9
Leonid Kondrashov <leo.kondrashov@gmail.com>
2024-05-01 15:43:11 +0800

    Increase timeout for CRI test deadline
    Signed-off-by: Leonid Kondrashov <leo.kondrashov@gmail.com>

--- cri/cri_test.go
+++ cri/cri_test.go
@@ -162,7 +162,7 @@ func invoke(t *testing.T, functionURL string) {
 	client, conn, err := getClient(functionURL)
 	require.NoError(t, err, "Failed to dial function URL")
 	defer conn.Close()
-	ctxFwd, cancel := context.WithDeadline(context.Background(), time.Now().Add(20*time.Second))
+	ctxFwd, cancel := context.WithDeadline(context.Background(), time.Now().Add(60*time.Second))
 	defer cancel()
 
 	resp, err := client.SayHello(ctxFwd, &hpb.HelloRequest{Name: reqPayload})
