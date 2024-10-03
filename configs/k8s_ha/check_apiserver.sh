ea7989b2fde043a0a6a9cc9f8be070aacafc6a06
Leonid Kondrashov <leo.kondrashov@gmail.com>
2024-10-03 11:31:59 +0800

    Fix api health check
    Signed-off-by: Leonid Kondrashov <leo.kondrashov@gmail.com>

--- configs/k8s_ha/check_apiserver.sh
+++ configs/k8s_ha/check_apiserver.sh
@@ -5,7 +5,7 @@ errorExit() {
     exit 1
 }
 
-curl --silent --max-time 2 --insecure https://localhost:8443/ -o /dev/null || errorExit "Error GET https://localhost:8443/"
+curl --silent --max-time 2 --insecure https://localhost:6443/ -o /dev/null || errorExit "Error GET https://localhost:6443/"
 if ip addr | grep -q 10.0.1.254; then
     curl --silent --max-time 2 --insecure https://10.0.1.254:8443/ -o /dev/null || errorExit "Error GET https://10.0.1.254:8443/"
 fi
