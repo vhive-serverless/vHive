ac36f25f8974d1e056febcd548343c3edb154b7b
Leonid Kondrashov <leo.kondrashov@gmail.com>
2025-11-07 12:09:15 +0000

    add setup script
    Signed-off-by: Leonid Kondrashov <leo.kondrashov@gmail.com>

---
+++ scripts/setup_pulsenet.sh
@@ -0,0 +1,19 @@
+sudo modprobe br_netfilter && sudo modprobe overlay && sudo sysctl -w net.ipv4.ip_forward=1 && sudo sysctl -w net.ipv4.conf.all.forwarding=1
+sudo sysctl -w net.bridge.bridge-nf-call-iptables=1 && sudo sysctl -w net.bridge.bridge-nf-call-ip6tables=1
+sudo mkdir -p /etc/firecracker-containerd && sudo mkdir -p /var/lib/firecracker-containerd/runtime && sudo mkdir -p /etc/containerd/
+
+sudo cp bin/{firecracker,jailer,containerd-shim-aws-firecracker,firecracker-containerd,firecracker-ctr} /usr/local/bin
+sudo cp bin/default-rootfs.img /var/lib/firecracker-containerd/runtime
+sudo cp bin/vmlinux-5.10.186 /var/lib/firecracker-containerd/runtime/hello-vmlinux.bin
+sudo cp configs/firecracker-containerd/config.toml /etc/firecracker-containerd/
+sudo cp configs/firecracker-containerd/firecracker-runtime.json /etc/containerd/
+
+./scripts/stargz/setup_demux_snapshotter.sh
+./scripts/stargz/setup_stargz.sh
+
+tmux new -s firecracker -d
+tmux send -t firecracker "sudo PATH=$PATH /usr/local/bin/firecracker-containerd --config /etc/firecracker-containerd/config.toml 2>&1 | tee ~/firecracker_log.txt" ENTER
+tmux new -s http-address-resolver -d
+tmux send -t http-address-resolver "sudo PATH=$PATH /usr/local/bin/http-address-resolver" ENTER
+tmux new -s demux-snapshotter -d
+tmux send -t demux-snapshotter 'while true; do sudo /usr/local/bin/demux-snapshotter; done' ENTER
