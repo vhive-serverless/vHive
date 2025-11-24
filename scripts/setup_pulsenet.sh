67bb11b0fbed7bb8d32c5bd74e45fc8534f89212
Leonid Kondrashov <leo.kondrashov@gmail.com>
2025-11-24 05:16:35 -0700

    fix vswarm relay
    Signed-off-by: Leonid Kondrashov <leo.kondrashov@gmail.com>

--- scripts/setup_pulsenet.sh
+++ scripts/setup_pulsenet.sh
@@ -21,4 +21,4 @@ tmux send -t http-address-resolver "sudo PATH=$PATH /usr/local/bin/http-address-
 tmux new -s demux-snapshotter -d
 tmux send -t demux-snapshotter 'while true; do sudo /usr/local/bin/demux-snapshotter; done' ENTER
 
-pushd ~; git clone https://github.com/vhive-serverless/vswarm; source /etc/profile; cd vswarm/tools/relay; make relay; popd
+pushd ~; git clone https://github.com/vhive-serverless/vswarm --branch lkondras/fix-relay; source /etc/profile; cd vswarm/tools/relay; make relay; popd
